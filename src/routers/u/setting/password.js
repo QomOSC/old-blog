f410a8ba65a2c683817d87f8513c84880c359bc6
Matin Kaboli <matinkaboli@aol.com>
2018-01-13 01:50:36 +0330

    fix: bugs for setting change password

--- src/routers/u/setting/password.js
+++ src/routers/u/setting/password.js
@@ -2,14 +2,18 @@ import { Router } from 'express';
 
 const { logged } = rootRequire('./perms');
 const { Member } = rootRequire('./models');
+const { crypt } = rootRequire('./utils');
 
 const router = new Router();
 
 router.post('/u/setting/password', logged, (req, res) => {
 
   Member.findOne({ _id: req.member.user._id }).then(member => {
-    if (req.body.password === member.password) {
-      member.password = req.body.newpassword;
+    const encryptedPass = crypt.decrypt(member.password, member.email);
+
+    if (req.body.password === encryptedPass) {
+      member.password =
+        crypt.encrypt(req.body.newpassword, member.email);
 
       member.save().then(() => {
         res.send('done');
@@ -17,7 +21,7 @@ router.post('/u/setting/password', logged, (req, res) => {
         res.send('err');
       });
     } else {
-      res.send('err');      
+      res.send('err');
     }
   }).catch(() => {
     res.send('err');
