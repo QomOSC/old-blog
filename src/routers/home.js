282225cde99541d8ee1ff60ef5cec26e18703ecd
Matin Kaboli <matinkaboli@aol.com>
2018-01-14 02:23:22 +0330

    feat: add moment-jalaali to project

--- src/routers/home.js
+++ src/routers/home.js
@@ -1,16 +1,66 @@
 import { Router } from 'express';
 
-const { Post } = rootRequire('./models');
+const { Post, Member } = rootRequire('./models');
+const { moment } = rootRequire('./utils');
 
 const router = new Router();
 
 router.get('/', (req, res) => {
 
-  Post.find({}).then(doc => {
-    res.render('home.njk', {
-      member: req.member.user,
-      doc
-    });
+  Post
+    .find({})
+    .sort({ createdAt: -1 })
+    .limit(20)
+    .then(doc => {
+
+    const posts = [];
+
+      function* getResponse() {
+        for (const i of doc) {
+          yield new Promise(resolve => {
+            const onePost = {
+              _id: i._id,
+              title: i.title,
+              createdAt: moment(i.createdAt),
+              content: i.content,
+              minutes: i.minutes,
+              likes: i.likes.length,
+              viewers: i.viewers.length,
+              author: {}
+            };
+
+            Member.findOne({ _id: i.author }).then(member => {
+              if (member) {
+                onePost.author.fname = member.fname;
+                onePost.author.lname = member.lname;
+                onePost.author.username = member.username;
+                posts.push(onePost);
+                resolve();
+              } else {
+                res.send('err');
+              }
+            }).catch(() => {
+              res.send('err');
+            });
+          });
+        }
+      }
+
+      const iterator = getResponse();
+      (function loop() {
+
+        const next = iterator.next();
+        if (next.done) {
+          res.render('home.njk', {
+            member: req.member.user,
+            posts
+          });
+          return;
+        }
+
+        next.value.then(loop);
+      }());
+
   }).catch(() => {
     res.send('err');
   });
--- src/routers/post/like.js
+++ src/routers/post/like.js
@@ -8,7 +8,7 @@ const router = new Router();
 router.post('/post/:id/like/', logged, (req, res) => {
   Post.findOne({ _id: req.params.id }).then(post => {
     if (post) {
-      
+
       if (post.likes.indexOf(req.member.user._id) === -1) {
         post.likes.push(req.member.user._id);
 
