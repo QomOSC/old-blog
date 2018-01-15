051efd171d0a6bb22d14eba669d57ad47034f865
Matin Kaboli <matinkaboli@aol.com>
2018-01-15 21:54:08 +0330

    feat: Add Samim Font to the project style /user/:username page (and responsive)

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
-const { Member } = rootRequire('./models');
+const { Member, Post } = rootRequire('./models');
+const { moment } = rootRequire('./utils');
 
 const router = new Router();
 
@@ -9,9 +10,51 @@ router.get('/user/:username', (req, res) => {
 
   Member.findOne({ username: req.params.username }).then(member => {
       if (member) {
-        res.render('user.njk', {
-          member
-        });
+
+        const user = [];
+
+        function* getResponse() {
+          yield new Promise(resolve => {
+            const userInfo = {
+              _id: member._id,
+              fname: member.fname,
+              lname: member.lname,
+              createdAt: moment(member.createdAt),
+              email: member.email,
+              username: member.username,
+              description: member.description,
+              avatar: member.avatar
+            };
+
+            user.push(userInfo);
+
+            Post.find({ author: member._id }).then(posts => {
+              user.push(posts);
+              resolve();
+            }).catch(() => {
+              res.json({ type: 2 });
+            });
+          });
+        }
+
+        const iterator = getResponse();
+        (function loop() {
+
+          const next = iterator.next();
+          if (next.done) {
+            console.log(user);
+
+            res.render('user.njk', {
+              member: user[0],
+              posts: user[1]
+            });
+
+            return;
+          }
+
+          next.value.then(loop);
+        }());
+
       } else {
         res.reply.notFound();
       }
