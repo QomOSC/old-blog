045ffdd0447cf4e2e8b2cf1d99fb18032b307729
Matin Kaboli <matinkaboli@aol.com>
2018-01-19 00:27:17 +0330

    feat: captcha for /signup

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -5,72 +5,111 @@ mainSetting.addEventListener('submit', e => {
 
   if (validateEmail(mainSetting.email.value)) {
 
-    fetch('/signup', {
-      method: 'POST',
-      credentials: 'include',
-      headers: new Headers({
-        'Content-Type': 'application/json'
-      }),
-      body: JSON.stringify({
-        fname: mainSetting.fname.value,
-        lname: mainSetting.lname.value,
-        username: mainSetting.username.value,
-        email: mainSetting.email.value,
-        password: mainSetting.password.value
-      })
-    }).then(checkStatus).then(res => res.json()).then(data => {
-      console.log(data);
-      if (data.type === 2) {
-        if (data.text === 0) {
-          iziToast.error({
-            title: 'خطا!',
-            rtl: true,
-            message: 'ایمیل توسط شخص دیگری گرفته شده'
-          });
-        } else if (data.text === 1) {
-          iziToast.error({
-            title: 'خطا!',
-            rtl: true,
-            message: 'موارد ضروری مقدار دهی نشده اند'
-          });
-        } else if (data.text === 2) {
-            iziToast.error({
-              title: 'خطا!',
+    if (validateUsername(mainSetting.username.value)) {
+
+      if (mainSetting.password.value.length >= 8) {
+
+        fetch('/signup', {
+          method: 'POST',
+          credentials: 'include',
+          headers: new Headers({
+            'Content-Type': 'application/json'
+          }),
+          body: JSON.stringify({
+            fname: mainSetting.fname.value,
+            lname: mainSetting.lname.value,
+            username: mainSetting.username.value,
+            email: mainSetting.email.value,
+            password: mainSetting.password.value,
+            captcha: mainSetting.captcha.value
+          })
+        }).then(checkStatus).then(res => res.json()).then(data => {
+          console.log(data);
+          if (data.type === 2) {
+            if (data.text === 0) {
+              iziToast.error({
+                title: 'خطا!',
+                rtl: true,
+                message: 'ایمیل توسط شخص دیگری گرفته شده'
+              });
+              mainSetting.email.select();
+            } else if (data.text === 1) {
+              iziToast.error({
+                title: 'خطا!',
+                rtl: true,
+                message: 'موارد ضروری مقدار دهی نشده اند'
+              });
+            } else if (data.text === 2) {
+                iziToast.error({
+                  title: 'خطا!',
+                  rtl: true,
+                  message: 'مشکلی پیش آمده، بعدا امتحان کنید'
+                });
+            } else if (data.text === 3) {
+              iziToast.error({
+                title: 'خطا!',
+                rtl: true,
+                message: 'یوزرنیم توسط شخص دیگری گرفته شده'
+              });
+              mainSetting.username.select();
+            } else if (data.text === 4) {
+              iziToast.error({
+                title: 'خطا!',
+                rtl: true,
+                message: 'ایمیل توسط شخص دیگری گرفته شده است'
+              });
+              mainSetting.email.select();
+            } else if (data.text === 5) {
+              iziToast.error({
+                title: 'خطا!',
+                rtl: true,
+                message: 'کد امنیتی اشتباه است'
+              });
+              mainSetting.captcha.select();
+            }
+          } else if (data.type === 0) {
+            iziToast.success({
+              title: 'موفق',
               rtl: true,
-              message: 'مشکلی پیش آمده، بعدا امتحان کنید'
+              message: 'حساب شما با موفقیت ساخته شد'
             });
-        } else if (data.text === 3) {
+          }
+        }).catch(() => {
           iziToast.error({
             title: 'خطا!',
             rtl: true,
-            message: 'یوزرنیم توسط شخص دیگری گرفته شده'
+            message: 'مشکلی پیش آمده، بعدا امتحان کنید'
           });
-        } else if (data.text === 4) {
-          iziToast.error({
-            title: 'خطا!',
-            rtl: true,
-            message: 'اییمل توسط شخص دیگری گرفته شده است'
-          });
-        }
-      } else if (data.type === 0) {
-        iziToast.success({
-          title: 'موفق',
-          rtl: true,
-          message: 'حساب شما با موفقیت ساخته شد'
         });
-      }
-    }).catch(() => {
-      iziToast.error({
-        title: 'خطا!',
+
+    } else {
+      iziToast.warning({
+        title: 'هشدار',
         rtl: true,
-        message: 'مشکلی پیش آمده، بعدا امتحان کنید'
+        message: 'رمز باید حداقل هشت رقم باشد'
       });
-    });
+      mainSetting.password.select();
+    }
+    } else {
+      iziToast.warning({
+        title: 'هشدار',
+        rtl: true,
+        message: 'یوزرنیم وارد شده اشتباه است'
+      });
+      mainSetting.username.select();
+    }
   } else {
     iziToast.warning({
       title: 'هشدار',
       rtl: true,
       message: 'ایمیل وارد شده اشتباه است'
     });
+    mainSetting.email.select();
   }
+});
+
+fetch('/captcha', {
+  credentials: 'include'
+}).then(checkStatus).then(res => res.json()).then(data => {
+  document.getElementById('svg-captcha').innerHTML = data.captcha;
 });
--- src/routers/signup.js
+++ src/routers/signup.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 const { login } = rootRequire('./perms');
 const { Member } = rootRequire('./models');
 const { crypt } = rootRequire('./utils');
+const { validator } = rootRequire('./utils');
 
 const router = new Router();
 
@@ -15,39 +16,53 @@ router.post('/signup', login, (req, res) => {
       req.body.lname &&
       req.body.email &&
       req.body.username &&
-      req.body.password) {
+      req.body.password &&
+      req.body.captcha) {
 
-    req.body.email = req.body.email.toLowerCase();
-    req.body.username = req.body.username.toLowerCase();
+    if (req.body.captcha === req.session.captcha) {
+      req.body.email = req.body.email.toLowerCase();
+      req.body.username = req.body.username.toLowerCase();
 
-    const member = new Member({
-      fname: req.body.fname,
-      lname: req.body.lname,
-      email: req.body.email,
-      username: req.body.username,
-      password: crypt.encrypt(req.body.password, req.body.email),
-      type: 1
-    });
+      if (validator.e(req.body.email) && validator.u(req.body.username)) {
+        const member = new Member({
+          fname: req.body.fname,
+          lname: req.body.lname,
+          email: req.body.email,
+          username: req.body.username,
+          password: crypt.encrypt(req.body.password, req.body.email),
+          type: 1
+        });
 
-    member.save().then(() => {
-      // OK
-      res.json({ type: 0 });
-    }).catch(() => {
-      const err = e.errmsg.split(' ');
+        member.save().then(() => {
+          // OK
+          req.session.captcha = null;
+          res.json({ type: 0 });
+        }).catch(() => {
+          const err = e.errmsg.split(' ');
 
-      if (err.includes('duplicate')) {
-        if (err.includes('username_1')) {
-          // Username Taken
-          res.json({ type: 2, text: 3 });
-        } else if (err.includes('email_1')) {
-          // Username Taken
-          res.json({ type: 2, text: 4 });
-        }
+          if (err.includes('duplicate')) {
+            if (err.includes('username_1')) {
+              // Username Taken
+              res.json({ type: 2, text: 3 });
+            } else if (err.includes('email_1')) {
+              // Username Taken
+              res.json({ type: 2, text: 4 });
+            }
+          } else {
+            // Error
+            res.json({ type: 2, text: 2 });
+          }
+        });
       } else {
-        // Error
-        res.json({ type: 2, text: 2 });
+        // Validate Error
+        res.json({ type: 2, text: 6 });
       }
-    });
+
+    } else {
+      // Wrong Captcha
+      res.json({ type: 2, text: 5 });
+    }
+
   } else {
     // Undefined Values
     res.json({ type: 2, text: 1 });
