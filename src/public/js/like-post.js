d6d011713830b8c165b056d5abce040cb769d795
Matin Kaboli <matinkaboli@aol.com>
2018-01-26 18:39:33 +0330

    fix: increment and decrement like number in viewpost

--- src/public/js/like-post.js
+++ src/public/js/like-post.js
@@ -1,7 +1,8 @@
 const likeDislike = document.getElementById('like-dislike-btn');
+const likeSpn = document.getElementById('like-and-dislike-number');
 
 likeDislike.addEventListener('click', () => {
-  if (likeDislike.getAttribute('data-status') === 'like') {
+  if (likeDislike.getAttribute('data-status') === 'disliked') {
     fetch(`/post/like/${likeDislike.getAttribute('data-id')}`, {
       method: 'POST',
       credentials: 'include',
@@ -10,15 +11,14 @@ likeDislike.addEventListener('click', () => {
         'Accept': 'application/json'
       })
     }).then(res => res.json()).then(data => {
-      console.log('Like');
-      console.log(data);
       if (data.type === 0) {
-        likeDislike.setAttribute('data-status', 'dislike');
+        likeDislike.setAttribute('data-status', 'liked');
         likeDislike.classList.remove('disliked');
         likeDislike.classList.add('liked');
 
         if (data.text === 0) {
           // Increment
+          likeSpn.innerHTML = parseInt(likeSpn.innerHTML) + 1;
         }
       } else if (data.type === 2) {
         iziToast.error({
@@ -34,7 +34,7 @@ likeDislike.addEventListener('click', () => {
         message: 'مشکلی پیش آمده، بعدا امتحان کنید'
       });
     });
-  } else if (likeDislike.getAttribute('data-status') === 'dislike') {
+  } else if (likeDislike.getAttribute('data-status') === 'liked') {
     fetch(`/post/dislike/${likeDislike.getAttribute('data-id')}`, {
       method: 'POST',
       credentials: 'include',
@@ -43,15 +43,14 @@ likeDislike.addEventListener('click', () => {
         'Accept': 'application/json'
       })
     }).then(res => res.json()).then(data => {
-      console.log('Dislike');
-      console.log(data);
       if (data.type === 0) {
-        likeDislike.setAttribute('data-status', 'like');
+        likeDislike.setAttribute('data-status', 'disliked');
         likeDislike.classList.remove('liked');
         likeDislike.classList.add('disliked');
 
         if (data.type === 0) {
           // Decrement
+          likeSpn.innerHTML = parseInt(likeSpn.innerHTML) - 1;
         }
       } else if (data.type === 2) {
         iziToast.error({
