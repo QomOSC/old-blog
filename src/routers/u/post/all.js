f40c6b605305aa5d1d390e7d54c05922a240e157
Matin Kaboli <matinkaboli@aol.com>
2018-01-28 16:46:48 +0330

    feat: send user posts to /u/post/all

--- src/routers/u/post/all.js
+++ src/routers/u/post/all.js
@@ -6,11 +6,62 @@ const { Post } = rootRequire('./models');
 const router = new Router();
 
 router.get('/u/post/all', logged, (req, res) => {
-  Post.find({ author: req.member.user._id }).then(authorposts => {
-    res.render('u/post/all.njk', {
-      member: req.member.user,
-      authorposts
-    });
+  Post
+    .find({ author: req.member.user._id })
+    .sort({ createdAt: -1 })
+    .limit(9)
+    .then(authorposts => {
+
+      if (JSON.stringify(authorposts) === '[]') {
+        res.render('u/post/all.njk', {
+          member: req.member.user,
+          empty: true
+        });
+      } else {
+
+        const posts = [];
+
+        function* getResponse() {
+          for (const i of authorposts) {
+            yield new Promise(resolve => {
+
+              let content = i.content.split('').slice(0, 130);
+              content.push('.', '.', '.');
+              content = content.join('');
+
+              const onePost = {
+                _id: i._id,
+                title: i.title,
+                likes: i.likes.length,
+                viewers: i.viewers.length,
+                avatar: i.avatar,
+                content
+              };
+
+              posts.push(onePost);
+
+              resolve();
+            });
+          }
+        }
+
+        const iterator = getResponse();
+        (function loop() {
+
+          const next = iterator.next();
+          if (next.done) {
+            res.render('u/post/all.njk', {
+              member: req.member.user,
+              posts
+            });
+            return;
+          }
+
+          next.value.then(loop);
+        })();
+
+      }
+
   }).catch(() => {
     res.reply.error();
   });
