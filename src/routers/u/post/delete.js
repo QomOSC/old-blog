5780f187a09a833ccb418f5200143a13c136a718
Matin Kaboli <matinkaboli@aol.com>
2018-01-28 17:36:11 +0330

    feat: post avatar will remove after removing post itself

--- src/routers/u/post/delete.js
+++ src/routers/u/post/delete.js
@@ -2,21 +2,30 @@ import { Router } from 'express';
 
 const { logged } = rootRequire('./perms');
 const { Post, Member } = rootRequire('./models');
+const { removeImage } = rootRequire('./utils');
 
 const router = new Router();
 
 router.post('/u/post/delete/:id', logged, (req, res) => {
-  Post.remove({
+  Post.findOne({
     author: req.member.user._id,
     _id: req.params.id
-  }).then(() => {
+  }).then(post => {
 
     Member.findOne({ _id: req.member.user._id }).then(member => {
       if (member) {
         member.posts.splice(req.params.id, 1);
 
         member.save().then(() => {
-          res.json({ type: 0 });
+
+          removeImage(post.avatar);
+
+          post.remove().then(() => {
+            res.json({ type: 0 });
+          }).catch(() => {
+            res.json({ type: 2 });
+          });
+
         }).catch(() => {
           res.json({ type: 2 });
         });
