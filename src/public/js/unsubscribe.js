9990cc50691aa0e65aaaec2373d7fe5f889a2edb
Matin Kaboli <matinkaboli@aol.com>
2018-02-02 21:26:14 +0330

    fix: use fetch to send data

--- src/public/js/unsubscribe.js
+++ src/public/js/unsubscribe.js
@@ -3,7 +3,60 @@ const unsub = document.forms['unsub-form'];
 unsub.addEventListener('submit', e => {
   e.preventDefault();
 
-  console.log(unsub.email.value);
+  if (validateEmail(unsub.email.value)) {
+    fetch('/unsubscribe', {
+      method: 'POST',
+      credentials: 'include',
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        'Accpet': 'application/json'
+      }),
+      body: JSON.stringify({
+        email: unsub.email.value,
+        captcha: unsub.captcha.value
+      })
+    }).then(checkStatus).then(res => res.json()).then(data => {
+      console.log(data);
+      if (data.type === 0) {
+        iziToast.success({
+          rtl: true,
+          title: 'شما با موفقیت خارج شددید'
+        });
+      } else if (data.type === 2) {
+        if (data.text === 0) {
+          iziToast.error({
+            rtl: 'true',
+            title: 'خطا',
+            message: 'کد امنیتی اشتباه است'
+          });
+        } else if (data.text === 1) {
+          iziToast.warning({
+            rtl: 'true',
+            title: 'اوپس',
+            message: 'کاربر در خبرنامه عضو نیست'
+          });
+        } else if (data.text === 2) {
+          iziToast.error({
+            rtl: 'true',
+            title: 'خطا',
+            message: 'مشکلی پیش آمده، بعدا امتحان کنید'
+          });
+        }
+      }
+    }).catch(() => {
+      iziToast.error({
+        rtl: 'true',
+        title: 'خطا',
+        message: 'مشکلی پیش آمده، بعدا امتحان کنید'
+      });
+    });
+  } else {
+    iziToast.warning({
+      rtl: 'true',
+      title: 'خطا!',
+      message: 'ایمیل معتبر نیست'
+    });
+  }
 });
 
 fetch('/captcha', {
