dbcfee0ef029489d6bd81a3690158f76402e9cff
Matin Kaboli <matinkaboli@aol.com>
2018-02-02 21:26:46 +0330

    feat: send json to be used with Fetch API

--- src/routers/newsletter/unsubscribe.js
+++ src/routers/newsletter/unsubscribe.js
@@ -9,7 +9,29 @@ router.get('/unsubscribe', (req, res) => {
 });
 
 router.post('/unsubscribe', (req, res) => {
-  
+  req.body.email = req.body.email.toLowerCase();
+  req.body.captcha = req.body.captcha.toLowerCase();
+
+  if (req.body.captcha === req.session.captcha) {
+
+    Newsletter.findOne({ email: req.body.email }).then(member => {
+      if (member) {
+        member.remove().then(() => {
+          req.session.captcha = null;
+          res.json({ type: 0 });
+        }).catch(() => {
+          // Error
+          res.json({ type: 2, text: 2 });
+        });
+      } else {
+        // User not found in Newsletter
+        res.json({ type: 2, text: 1 });
+      }
+    });
+  } else {
+    // Wrong Captcha
+    res.json({ type: 2, text: 0 });
+  }
 });
 
 export default router;
