2978b5138ee2922943952c042851f6b85b4adf55
Matin Kaboli <matinkaboli@aol.com>
2018-02-05 21:31:38 +0330

    feat: send admin info to /about

--- src/routers/about.js
+++ src/routers/about.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 
+const { Member } = rootRequire('./models');
+
 const router = new Router();
 
 router.get('/about', (req, res) => {
-  res.render('about.njk');
+  Member.findOne({ username: 'matinkaboli' }).then(member => {
+    res.render('about.njk', {
+      member
+    });
+  }).catch(() => {
+    res.render('about.njk');
+  });
 });
 
 export default router;
