50f4ab48013c9960112cb3c03c1af6a744771cb9
Matin Kaboli <matinkaboli@aol.com>
2018-02-08 23:49:45 +0330

    feat: add getQuery function to commonfuncs file

--- src/public/js/common-funcs.js
+++ src/public/js/common-funcs.js
@@ -5,3 +5,22 @@ function iziErr() {
     message: 'مشکلی پیش آمده، بعدا امتحان کنید'
   });
 }
+
+function getQuery(name, url) {
+    if (!url) {
+      url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, '\\$&');
+
+    let regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`),
+        results = regex.exec(url);
+
+    if (!results) {
+      return null;
+    }
+    if (!results[2]) {
+      return '';
+    }
+
+    return decodeURIComponent(results[2].replace(/\+/g, ''));
+}
