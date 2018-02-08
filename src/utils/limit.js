549917efab4c7a383bb54cf76c34967d5e2088be
Matin Kaboli <matinkaboli@aol.com>
2018-02-08 12:47:00 +0330

    fix: set delay of rate-limit to 0

--- src/utils/limit.js
+++ src/utils/limit.js
@@ -3,7 +3,7 @@ import RateLimit from 'express-rate-limit';
 const limit = new RateLimit({
   windowMs: 60 * 60 * 1000 * 2,
   delayAfter: 0,
-  delayMs: 200,
+  delayMs: 0,
   max: 50,
   message: 'درخواست بیش از حد، بعدا امتحان کنید'
 });
