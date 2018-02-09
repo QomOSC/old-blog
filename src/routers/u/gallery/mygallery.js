6b5c5f9ae460d35ad48fdfcbe0cd899c9b920887
Matin Kaboli <matinkaboli@aol.com>
2018-02-09 12:49:34 +0330

    feat: show 12 first photo in /u/gallery

--- src/routers/u/gallery/mygallery.js
+++ src/routers/u/gallery/mygallery.js
@@ -2,14 +2,66 @@ import { Router } from 'express';
 
 const { logged } = rootRequire('./perms');
 const { Gallery } = rootRequire('./models');
+const { moment } = rootRequire('./utils');
 
 const router = new Router();
 
 router.get('/u/gallery/', logged, (req, res) => {
-  Gallery.find({ photographer: req.member.user._id }).then(photos => {
-    res.render('u/gallery/mygallery.njk', {
-      photos
-    });
+  const page = Math.abs(parseInt(req.query.page) - 1);
+  let start, stop;
+
+  if (page.toString() !== NaN.toString()) {
+    start = page * 12;
+    stop = page * 12 + 12;
+  } else {
+    start = 0;
+    stop = 12;
+  }
+
+  Gallery
+  .find({ photographer: req.member.user._id })
+  .skip(start)
+  .limit(stop)
+  .then(photos => {
+    if (JSON.stringify(photos) === '[]') {
+      res.render('u/gallery/mygallery.njk', {
+        empty: true
+      });
+    } else {
+
+      const allPhotos = [];
+
+      function* getResponse() {
+        for (const i of photos) {
+          yield new Promise(resolve => {
+
+            const onePhoto = {
+              _id: i._id,
+              title: i.title,
+              photo: i.photo,
+              createdAt: moment(i.createdAt),
+              author: {},
+            };
+            allPhotos.push(onePhoto);
+            resolve();
+          });
+        }
+      }
+
+      const iterator = getResponse();
+      (function loop() {
+
+        const next = iterator.next();
+        if (next.done) {
+          res.render('u/gallery/mygallery.njk', {
+            photos: allPhotos
+          });
+          return;
+        }
+
+        next.value.then(loop);
+      })();
+    }
   });
 });
 
