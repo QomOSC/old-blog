2b1c986d871e5259748e285c5d6fa58b9a25692c
Matin Kaboli <matinkaboli@aol.com>
2018-02-09 16:11:31 +0330

    feat: search in conference

--- src/routers/conferences.js
+++ src/routers/conferences.js
@@ -6,13 +6,29 @@ const { moment } = rootRequire('./utils');
 const router = new Router();
 
 router.get('/conferences', (req, res) => {
-  Conference
-    .find({ type: { $in: [3, 4] } })
-    .sort({ createdAt: -1 })
-    .limit(20)
-    .then(confs => {
-      if (JSON.stringify(confs) === '[]') {
+  const page = Math.abs(parseInt(req.query.page) - 1);
+  let start, stop;
+
+  if (page.toString() !== NaN.toString()) {
+    start = page * 12;
+    stop = page * 12 + 12;
+  } else {
+    start = 0;
+    stop = 12;
+  }
+
+  if (req.query.q) {
+    const re = new RegExp(`.*${req.query.q}.*`);
+
+    Conference
+      .find({ description: re, type: { $in: [3, 4] } })
+      .skip(start)
+      .limit(stop)
+      .then(confs => {
+      if (confs.length === 0) {
         res.render('conferences.njk', {
+          type: 1,
+          query: req.query.q,
           empty: true
         });
       } else {
@@ -56,7 +72,71 @@ router.get('/conferences', (req, res) => {
           const next = iterator.next();
           if (next.done) {
             res.render('conferences.njk', {
-              confs: confArr
+              confs: confArr,
+              type: 1,
+              query: req.query.q,
+            });
+            return;
+          }
+
+          next.value.then(loop);
+        })();
+      }
+    });
+  } else {
+  Conference
+    .find({ type: { $in: [3, 4] } })
+    .sort({ createdAt: -1 })
+    .limit(20)
+    .then(confs => {
+      if (JSON.stringify(confs) === '[]') {
+        res.render('conferences.njk', {
+          empty: true,
+          type: 0
+        });
+      } else {
+
+        const confArr = [];
+
+        function* getResponse() {
+          for (const i of confs) {
+            yield new Promise(resolve => {
+
+              const oneConf = {
+                title: i.title,
+                createdAt: moment(i.createdAt),
+                description: i.description,
+                avatar: i.avatar,
+                provider: {},
+              };
+
+              Member.findOne({ _id: i.provider }).then(member => {
+                if (member) {
+                  oneConf.provider.fname = member.fname;
+                  oneConf.provider.lname = member.lname;
+                  oneConf.provider.username = member.username;
+                  oneConf.provider.avatar = member.avatar;
+
+                  confArr.push(oneConf);
+                  resolve();
+                } else {
+                  res.reply.error();
+                }
+              }).catch(() => {
+                res.reply.error();
+              });
+            });
+          }
+        }
+
+        const iterator = getResponse();
+        (function loop() {
+
+          const next = iterator.next();
+          if (next.done) {
+            res.render('conferences.njk', {
+              confs: confArr,
+              type: 0
             });
             return;
           }
@@ -67,9 +147,11 @@ router.get('/conferences', (req, res) => {
       }
     }).catch(() => {
       res.render('conferences.njk', {
-        empty: true
+        empty: true,
+        type: 0
       });
     });
+  }
 });
 
 export default router;
