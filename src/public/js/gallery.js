b87089ea35b7c8d7593d4eec3a5d68306218f2c4
Matin Kaboli <matinkaboli@aol.com>
2018-02-09 12:43:32 +0330

    feat: send user information to /gallery

--- src/public/js/gallery.js
+++ src/public/js/gallery.js
@@ -1,13 +1,14 @@
-const page = parseInt(getQuery('page')) || 0;
+const page = parseInt(getQuery('page') - 1);
 const start = page * 12,
       stop = page * 12 + 12;
 
 const previous = document.getElementById('previous'),
       next = document.getElementById('next');
 
-const previousLink = `${window.location.origin}/gallery?page=${page - 1}`;
-const nextLink = `${window.location.origin}/gallery?page=${page + 1}`
+const previousLink = `${window.location.origin}/gallery?page=${page - 2}`;
+const nextLink = `${window.location.origin}/gallery?page=${page + 2}`;
 
+console.log(start, stop);
 if (start <= 0) {
   previous.style.display = 'none';
 }
--- src/routers/gallery.js
+++ src/routers/gallery.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
-const { Gallery } = rootRequire('./models');
+const { Gallery, Member } = rootRequire('./models');
+const { moment } = rootRequire('./utils');
 
 const router = new Router();
 
@@ -27,9 +28,53 @@ router.get('/gallery', (req, res) => {
           empty: true
         });
       } else {
-        res.render('gallery.njk', {
-          photos
-        });
+
+        const allPhotos = [];
+
+        function* getResponse() {
+          for (const i of photos) {
+            yield new Promise(resolve => {
+
+              const onePhoto = {
+                _id: i._id,
+                title: i.title,
+                photo: i.photo,
+                createdAt: moment(i.createdAt),
+                author: {},
+              };
+
+              Member.findOne({ _id: i.photographer }).then(member => {
+                if (member) {
+                  onePhoto.author.fname = member.fname;
+                  onePhoto.author.lname = member.lname;
+                  onePhoto.author.username = member.username;
+                  onePhoto.author.avatar = member.avatar;
+
+                  allPhotos.push(onePhoto);
+                  resolve();
+                } else {
+                  res.reply.error();
+                }
+              }).catch(() => {
+                res.reply.error();
+              });
+            });
+          }
+        }
+
+        const iterator = getResponse();
+        (function loop() {
+
+          const next = iterator.next();
+          if (next.done) {
+            res.render('gallery.njk', {
+              photos: allPhotos
+            });
+            return;
+          }
+
+          next.value.then(loop);
+        })();
       }
     }).catch(() => {
       res.reply.notFound();
