7b333f00dfa529aff043431ece0f3eb3237d9e4e
Matin Kaboli <matinkaboli@aol.com>
2018-02-10 16:34:49 +0330

    feat: show last conference in home page

--- src/routers/home.js
+++ src/routers/home.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-const { Post, Member } = rootRequire('./models');
+const { Post, Member, Conference } = rootRequire('./models');
 const { moment } = rootRequire('./utils');
 
 const router = new Router();
@@ -20,6 +20,7 @@ router.get('/', (req, res) => {
       } else {
 
         const posts = [];
+        const lastConf = { provider: {} };
 
         function* getResponse() {
           for (const i of doc) {
@@ -56,6 +57,36 @@ router.get('/', (req, res) => {
               });
             });
           }
+          yield new Promise(resolve => {
+          Conference
+            .find({ type: { $in: [3, 4] } })
+            .sort({ createdAt: -1 })
+            .limit(1)
+            .then(conf => {
+              if (conf.length === 0) {
+                lastConf.empty = true;
+                resolve();
+              } else {
+                lastConf.title = conf[0].title;
+                lastConf.description = conf[0].description;
+                lastConf.avatar = conf[0].avatar;
+                lastConf.createdAt = moment(conf[0].createdAt);
+                Member.findOne({ _id: conf[0].provider }).then(provider => {
+                  if (provider) {
+                    lastConf.provider.fname = provider.fname;
+                    lastConf.provider.lname = provider.lname;
+                    lastConf.provider.username = provider.username;
+                    lastConf.provider.avatar = provider.avatar;
+                  }
+                  resolve();
+                }).catch(e => {
+                  console.log(e);
+                });
+              }
+            }).catch(e => {
+              console.log(e);
+            });
+          });
         }
 
         const iterator = getResponse();
@@ -64,7 +95,8 @@ router.get('/', (req, res) => {
           const next = iterator.next();
           if (next.done) {
             res.render('home.njk', {
-              posts
+              posts,
+              lastConf
             });
             return;
           }
