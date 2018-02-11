c9b07fe10537744a79f3b30ab30bbcf8319f0511
Matin Kaboli <matinkaboli@aol.com>
2018-02-11 13:43:29 +0330

    fix: show the real provider of conference than MSM

--- src/routers/u/conference/manage/manage.js
+++ src/routers/u/conference/manage/manage.js
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 
-const { Conference } = rootRequire('./models');
+const { Conference, Member } = rootRequire('./models');
 const perms = rootRequire('./perms');
+const { moment } = rootRequire('./utils');
 
 const router = new Router();
 router.get(
@@ -10,11 +11,58 @@ router.get(
   perms.u.admin,
   (req, res) => {
     Conference.find({ type: 1 }).then(confs => {
-      if (JSON.stringify(confs) !== '[]') {
-        res.render('u/conference/manage/manage.njk', {
-          member: req.member.user,
-          confs
-        });
+      if (confs.length !== 0) {
+
+        const allConfs = [];
+        function* getResponse() {
+
+          for (const i of confs) {
+            yield new Promise(resolve => {
+              let description = i.description.split('').slice(0, 130);
+              description.push('.', '.', '.');
+              description = description.join('');
+
+              const oneConf = {
+                _id: i._id,
+                title: i.title,
+                createdAt: moment(i.createdAt),
+                author: {},
+                description
+              };
+
+              Member.findOne({ _id: i.provider }).then(member => {
+                if (member) {
+                  oneConf.author.fname = member.fname;
+                  oneConf.author.lname = member.lname;
+                  oneConf.author.username = member.username;
+                  oneConf.author.avatar = member.avatar;
+
+                  allConfs.push(oneConf);
+                  resolve();
+                } else {
+                  res.reply.error();
+                }
+              }).catch(() => {
+                res.reply.error();
+              });
+            });
+          }
+        }
+
+        const iterator = getResponse();
+        (function loop() {
+
+          const next = iterator.next();
+          if (next.done) {
+            res.render('u/conference/manage/manage.njk', {
+              member: req.member.user,
+              confs: allConfs
+            });
+            return;
+          }
+
+          next.value.then(loop);
+        })();
       } else {
         res.render('u/conference/manage/manage.njk', {
           member: req.member.user,
