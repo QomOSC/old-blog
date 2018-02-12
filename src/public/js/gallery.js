a8ec7a8f988c6a833fc8397e0bc1d7b63f1551a6
Matin Kaboli <matinkaboli@aol.com>
2018-02-12 22:40:25 +0330

    style: lint gallery.js in public/js

--- src/public/js/gallery.js
+++ src/public/js/gallery.js
@@ -23,7 +23,6 @@ if (getQuery('q')) {
 if (start <= 0) {
   previous.style.display = 'none';
 }
-console.log(cnf.length);
 
 if (cnf.length < 12) {
   next.style.display = 'none';
@@ -33,22 +32,23 @@ previous.setAttribute('href', previousLink);
 next.setAttribute('href', nextLink);
 
 // Get the modal
-var modal = document.getElementById('myModal');
+let modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-    modal.style.display = "block";
+let img = document.getElementById('myImg');
+let modalImg = document.getElementById('img01');
+let captionText = document.getElementById('caption');
+
+img.onclick = function() {
+    modal.style.display = 'block';
     modalImg.src = this.src;
     captionText.innerHTML = this.alt;
-}
+};
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName('close')[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-    modal.style.display = "none";
-}
+    modal.style.display = 'none';
+};
