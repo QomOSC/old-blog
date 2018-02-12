5a8644a15e9be61bc62893bbc7d8322cb8f0a664
Matin Kaboli <matinkaboli@aol.com>
2018-02-13 02:16:24 +0330

    fix: picture modal

--- src/public/js/gallery.js
+++ src/public/js/gallery.js
@@ -35,14 +35,23 @@ next.setAttribute('href', nextLink);
 let modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-let img = document.getElementById('myImg');
+let img = document.getElementsByClassName('myImg');
+
 let modalImg = document.getElementById('img01');
 let captionText = document.getElementById('caption');
 
-img.onclick = function() {
-    modal.style.display = 'block';
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
+
+for (let i = 0; i < img.length; i++) {
+  img[i].onclick = function() {
+      modal.style.display = 'block';
+      modalImg.src = this.src;
+      captionText.innerHTML = this.alt;
+
+  };
+}
+
+modal.onclick = function() {
+  modal.style.display = 'none';
 };
 
 // Get the <span> element that closes the modal
@@ -50,5 +59,5 @@ let span = document.getElementsByClassName('close')[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-    modal.style.display = 'none';
+  modal.style.display = 'none';
 };
