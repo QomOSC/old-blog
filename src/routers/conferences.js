96ff49cee6d9e9e363d92af64df87a97154f00aa
Matin Kaboli <matinkaboli@aol.com>
2018-02-13 21:49:49 +0330

    feat: use async/await in conferences and gallery page

--- src/routers/conferences.js
+++ src/routers/conferences.js
@@ -5,90 +5,87 @@ const { moment } = rootRequire('./utils');
 
 const router = new Router();
 
-router.get('/conferences', (req, res) => {
+router.get('/conferences', async(req, res) => {
   const page = parseInt(req.query.page) || 0;
   const start = page * 12,
         stop = page * 12 + 12;
 
   const re = new RegExp(`.*${req.query.q || ''}.*`);
 
-  Conference
+  const confs = await Conference
     .find({ description: re, type: { $in: [3, 4] } })
     .skip(start)
-    .limit(stop)
-    .then(confs => {
-    if (confs.length === 0) {
-      if (req.query.q) {
-        res.render('conferences.njk', {
-          type: 1,
-          query: req.query.q,
-          empty: true
-        });
-      } else {
-        res.render('conferences.njk', {
-          type: 0,
-          empty: true
+    .limit(stop);
+
+  if (confs.length === 0) {
+    if (req.query.q) {
+      res.render('conferences.njk', {
+        type: 1,
+        query: req.query.q,
+        empty: true
+      });
+    } else {
+      res.render('conferences.njk', {
+        type: 0,
+        empty: true
+      });
+    }
+  } else {
+
+    const confArr = [];
+
+    function* getResponse() {
+      for (const i of confs) {
+        yield new Promise(async resolve => {
+
+          const oneConf = {
+            title: i.title,
+            createdAt: moment(i.createdAt),
+            description: i.description,
+            avatar: i.avatar,
+            provider: {},
+          };
+
+          const member = Member.findOne({ _id: i.provider });
+
+          if (member) {
+            oneConf.provider.fname = member.fname;
+            oneConf.provider.lname = member.lname;
+            oneConf.provider.username = member.username;
+            oneConf.provider.avatar = member.avatar;
+
+            confArr.push(oneConf);
+            resolve();
+          } else {
+            res.reply.error();
+          }
         });
       }
-    } else {
+    }
 
-      const confArr = [];
+    const iterator = getResponse();
+    (function loop() {
 
-      function* getResponse() {
-        for (const i of confs) {
-          yield new Promise(resolve => {
-
-            const oneConf = {
-              title: i.title,
-              createdAt: moment(i.createdAt),
-              description: i.description,
-              avatar: i.avatar,
-              provider: {},
-            };
-
-            Member.findOne({ _id: i.provider }).then(member => {
-              if (member) {
-                oneConf.provider.fname = member.fname;
-                oneConf.provider.lname = member.lname;
-                oneConf.provider.username = member.username;
-                oneConf.provider.avatar = member.avatar;
-
-                confArr.push(oneConf);
-                resolve();
-              } else {
-                res.reply.error();
-              }
-            }).catch(() => {
-              res.reply.error();
-            });
+      const next = iterator.next();
+      if (next.done) {
+        if (req.query.q) {
+          res.render('conferences.njk', {
+            confs: confArr,
+            type: 1,
+            query: req.query.q,
+          });
+        } else {
+          res.render('conferences.njk', {
+            confs: confArr,
+            type: 0
           });
         }
+        return;
       }
 
-      const iterator = getResponse();
-      (function loop() {
-
-        const next = iterator.next();
-        if (next.done) {
-          if (req.query.q) {
-            res.render('conferences.njk', {
-              confs: confArr,
-              type: 1,
-              query: req.query.q,
-            });
-          } else {
-            res.render('conferences.njk', {
-              confs: confArr,
-              type: 0
-            });
-          }
-          return;
-        }
-
-        next.value.then(loop);
-      })();
-    }
-  });
+      next.value.then(loop);
+    })();
+  }
 });
 
 export default router;
--- src/routers/gallery.js
+++ src/routers/gallery.js
@@ -5,93 +5,88 @@ const { moment } = rootRequire('./utils');
 
 const router = new Router();
 
-router.get('/gallery', (req, res) => {
+router.get('/gallery', async(req, res) => {
   const page = parseInt(req.query.page) || 0;
   const start = page * 12,
         stop = page * 12 + 12;
 
   const re = new RegExp(`.*${req.query.q || ''}.*`);
 
-  Gallery
-    .find({ title: re })
-    .sort({ createdAt: -1 })
-    .skip(start)
-    .limit(stop)
-    .then(photos => {
-      if (photos.length === 0) {
+  const photos = await Gallery
+      .find({ title: re })
+      .sort({ createdAt: -1 })
+      .skip(start)
+      .limit(stop);
+
+  if (photos.length === 0) {
+    if (req.query.q) {
+      res.render('gallery.njk', {
+        type: 1,
+        query: req.query.q,
+        empty: true
+      });
+    } else {
+      res.render('gallery.njk', {
+        type: 0,
+        empty: true
+      });
+    }
+  } else {
+
+    const allPhotos = [];
+
+    function* getResponse() {
+      for (const i of photos) {
+        yield new Promise(async resolve => {
+
+          const onePhoto = {
+            _id: i._id,
+            title: i.title,
+            photo: i.photo,
+            createdAt: moment(i.createdAt),
+            author: {},
+          };
+
+          const member = await Member.findOne({ _id: i.photographer });
+
+          if (member) {
+            onePhoto.author.fname = member.fname;
+            onePhoto.author.lname = member.lname;
+            onePhoto.author.username = member.username;
+            onePhoto.author.avatar = member.avatar;
+
+            allPhotos.push(onePhoto);
+            resolve();
+          } else {
+            res.reply.error();
+          }
+        });
+      }
+    }
+
+    const iterator = getResponse();
+    (function loop() {
+
+      const next = iterator.next();
+      if (next.done) {
         if (req.query.q) {
           res.render('gallery.njk', {
+            photos: allPhotos,
             type: 1,
             query: req.query.q,
-            empty: true
           });
         } else {
           res.render('gallery.njk', {
-            type: 0,
-            empty: true
+            photos: allPhotos,
+            type: 0
           });
         }
-      } else {
-
-        const allPhotos = [];
-
-        function* getResponse() {
-          for (const i of photos) {
-            yield new Promise(resolve => {
-
-              const onePhoto = {
-                _id: i._id,
-                title: i.title,
-                photo: i.photo,
-                createdAt: moment(i.createdAt),
-                author: {},
-              };
-
-              Member.findOne({ _id: i.photographer }).then(member => {
-                if (member) {
-                  onePhoto.author.fname = member.fname;
-                  onePhoto.author.lname = member.lname;
-                  onePhoto.author.username = member.username;
-                  onePhoto.author.avatar = member.avatar;
-
-                  allPhotos.push(onePhoto);
-                  resolve();
-                } else {
-                  res.reply.error();
-                }
-              }).catch(() => {
-                res.reply.error();
-              });
-            });
-          }
-        }
-
-        const iterator = getResponse();
-        (function loop() {
-
-          const next = iterator.next();
-          if (next.done) {
-            if (req.query.q) {
-              res.render('gallery.njk', {
-                photos: allPhotos,
-                type: 1,
-                query: req.query.q,
-              });
-            } else {
-              res.render('gallery.njk', {
-                photos: allPhotos,
-                type: 0
-              });
-            }
-            return;
-          }
-
-          next.value.then(loop);
-        })();
+        return;
       }
-    }).catch(() => {
-      res.reply.notFound();
-    });
+
+      next.value.then(loop);
+    })();
+  }
 });
 
 export default router;
