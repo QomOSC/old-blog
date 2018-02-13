626b294cf89579fcce702b17bcf088d3a6fd7b96
Matin Kaboli <matinkaboli@aol.com>
2018-02-13 22:24:08 +0330

    feat: use async/await in edit.js

--- src/routers/u/post/edit.js
+++ src/routers/u/post/edit.js
@@ -5,47 +5,44 @@ const { logged } = rootRequire('./perms');
 
 const router = new Router();
 
-router.get('/u/post/:id/edit', logged, (req, res) => {
-  Post.findOne({ _id: req.params.id }).then(post => {
-    if (post && post.author.toString() === req.member.user._id.toString()) {
-      res.render('u/post/edit.njk', {
-        post,
-        member: req.member.user
-      });
-    } else {
-      res.reply.notFound();
-    }
-  }).catch(() => {
+router.get('/u/post/:id/edit', logged, async(req, res) => {
+
+  const post = await Post.findOne({ _id: req.params.id });
+
+  if (post && post.author.toString() === req.member.user._id.toString()) {
+    res.render('u/post/edit.njk', {
+      post,
+      member: req.member.user
+    });
+  } else {
     res.reply.notFound();
-  });
+  }
 });
 
 router.post(
   '/u/post/edit',
   logged,
-  (req, res) => {
+  async(req, res) => {
 
   if (req.body.title && req.body.content && req.body.minutes && req.body.id) {
-    Post.findOne({
+
+    const post = await Post.findOne({
       _id: req.body.id,
       author: req.member.user._id.toString()
-    }).then(post => {
-      if (post) {
-        post.title = req.body.title;
-        post.content = req.body.content;
-        post.minutes = req.body.minutes;
-
-        post.save().then(() => {
-          res.json({ type: 0 });
-        }).catch(() => {
-          // Error
-          res.json({ type: 2, text: 0 });
-        });
-      }
-    }).catch(() => {
-      // Error
-      res.json({ type: 2, text: 0 });
     });
+    
+    if (post) {
+      post.title = req.body.title;
+      post.content = req.body.content;
+      post.minutes = req.body.minutes;
+
+      post.save().then(() => {
+        res.json({ type: 0 });
+      }).catch(() => {
+        // Error
+        res.json({ type: 2, text: 0 });
+      });
+    }
   } else {
     // Error
     res.json({ type: 2, text: 0 });
