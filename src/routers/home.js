956b4b979a670abd59a590f5f3e6619b33cd4670
Matin Kaboli <matinkaboli@aol.com>
2018-02-13 21:44:31 +0330

    feat: use async/await for home page and login page

--- src/routers/home.js
+++ src/routers/home.js
@@ -5,109 +5,95 @@ const { moment } = rootRequire('./utils');
 
 const router = new Router();
 
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
 
-  Post
-    .find({})
-    .sort({ createdAt: -1 })
-    .limit(20)
-    .then(doc => {
+  const doc = await Post.find({}).sort({ createdAt: -1 }).limit(20);
 
-      if (JSON.stringify(doc) === '[]') {
-        res.render('home.njk', {
-          empty: true
+  if (doc.length === 0) {
+    res.render('home.njk', {
+      empty: true
+    });
+  } else {
+
+    const posts = [];
+    const lastConf = { provider: {} };
+
+    function* getResponse() {
+      for (const i of doc) {
+
+        yield new Promise(async resolve => {
+          let content = i.content.split('').slice(0, 130);
+          content.push('.', '.', '.');
+          content = content.join('');
+
+          const onePost = {
+            _id: i._id,
+            title: i.title,
+            createdAt: moment(i.createdAt),
+            likes: i.likes.length,
+            viewers: i.viewers.length,
+            avatar: i.avatar,
+            author: {},
+            content
+          };
+
+          const member = await Member.findOne({ _id: i.author });
+
+          if (member) {
+            onePost.author.fname = member.fname;
+            onePost.author.lname = member.lname;
+            onePost.author.username = member.username;
+            onePost.author.avatar = member.avatar;
+
+            posts.push(onePost);
+            resolve();
+          } else {
+            res.reply.error();
+          }
         });
-      } else {
-
-        const posts = [];
-        const lastConf = { provider: {} };
-
-        function* getResponse() {
-          for (const i of doc) {
-            yield new Promise(resolve => {
-              let content = i.content.split('').slice(0, 130);
-              content.push('.', '.', '.');
-              content = content.join('');
-
-              const onePost = {
-                _id: i._id,
-                title: i.title,
-                createdAt: moment(i.createdAt),
-                likes: i.likes.length,
-                viewers: i.viewers.length,
-                avatar: i.avatar,
-                author: {},
-                content
-              };
-
-              Member.findOne({ _id: i.author }).then(member => {
-                if (member) {
-                  onePost.author.fname = member.fname;
-                  onePost.author.lname = member.lname;
-                  onePost.author.username = member.username;
-                  onePost.author.avatar = member.avatar;
-
-                  posts.push(onePost);
-                  resolve();
-                } else {
-                  res.reply.error();
-                }
-              }).catch(() => {
-                res.reply.error();
-              });
-            });
-          }
-          yield new Promise(resolve => {
-          Conference
-            .find({ type: { $in: [3, 4] } })
-            .sort({ createdAt: -1 })
-            .limit(1)
-            .then(conf => {
-              if (conf.length === 0) {
-                lastConf.empty = true;
-                resolve();
-              } else {
-                lastConf.title = conf[0].title;
-                lastConf.description = conf[0].description;
-                lastConf.avatar = conf[0].avatar;
-                lastConf.createdAt = moment(conf[0].createdAt);
-                Member.findOne({ _id: conf[0].provider }).then(provider => {
-                  if (provider) {
-                    lastConf.provider.fname = provider.fname;
-                    lastConf.provider.lname = provider.lname;
-                    lastConf.provider.username = provider.username;
-                    lastConf.provider.avatar = provider.avatar;
-                  }
-                  resolve();
-                }).catch(e => {
-                  console.log(e);
-                });
-              }
-            }).catch(e => {
-              console.log(e);
-            });
-          });
-        }
-
-        const iterator = getResponse();
-        (function loop() {
-
-          const next = iterator.next();
-          if (next.done) {
-            res.render('home.njk', {
-              posts,
-              lastConf
-            });
-            return;
-          }
-
-          next.value.then(loop);
-        })();
       }
 
-  }).catch(() => {
-    res.reply.error();
-  });
+      yield new Promise(async resolve => {
+
+        const conf = await Conference
+                .find({ type: { $in: [3, 4] } })
+                .sort({ createdAt: -1 })
+                .limit(1);
+
+        if (conf.length === 0) {
+          lastConf.empty = true;
+          resolve();
+        } else {
+          lastConf.title = conf[0].title;
+          lastConf.description = conf[0].description;
+          lastConf.avatar = conf[0].avatar;
+          lastConf.createdAt = moment(conf[0].createdAt);
+
+          const provider = await Member.findOne({ _id: conf[0].provider });
+
+          if (provider) {
+            lastConf.provider.fname = provider.fname;
+            lastConf.provider.lname = provider.lname;
+            lastConf.provider.username = provider.username;
+            lastConf.provider.avatar = provider.avatar;
+          }
+          resolve();
+        }
+      });
+    }
+
+    const iterator = getResponse();
+    (function loop() {
+
+      const next = iterator.next();
+      if (next.done) {
+        res.render('home.njk', { posts, lastConf });
+        return;
+      }
+
+      next.value.then(loop);
+    })();
+  }
 });
 
 export default router;
--- src/routers/login.js
+++ src/routers/login.js
@@ -10,39 +10,37 @@ router.get('/login', login, (req, res) => {
   res.render('login.njk');
 });
 
-router.post('/login', login, (req, res) => {
+router.post('/login', login, async(req, res) => {
   req.body.email = req.body.email.toLowerCase();
   req.body.captcha = req.body.captcha.toLowerCase();
 
   if (req.body.captcha === req.session.captcha) {
-    Member.findOne({
-      email: req.body.email
-    }).then(member => {
-      if (member) {
 
-        if (crypt.decrypt(member.password, member.email) ===
-        req.body.password) {
-            if (member.type === 1) {
-              // Account is deactive
-              res.json({ type: 1, text: 0 });
-            } else {
-              req.member.login(member);
-              // OK
-              req.session.captcha = null;
-              res.json({ type: 0 });
-            }
+    const member = await Member.findOne({ email: req.body.email });
+
+    if (member) {
+
+      if (crypt.decrypt(member.password, member.email) ===
+      req.body.password) {
+
+        if (member.type === 1) {
+          // Account is deactive
+          res.json({ type: 1, text: 0 });
         } else {
-          // Wrong Password
-          res.json({ type: 2, text: 0 });
+          req.member.login(member);
+          // OK
+          req.session.captcha = null;
+          res.json({ type: 0 });
         }
+
       } else {
-        // User Not Found
-        res.json({ type: 2, text: 1 });
+        // Wrong Password
+        res.json({ type: 2, text: 0 });
       }
-    }).catch(() => {
-      // Error
-      res.json({ type: 2, text: 2 });
-    });
+    } else {
+      // User Not Found
+      res.json({ type: 2, text: 1 });
+    }
   } else {
     // Wrong Captcha
     res.json({ type: 2, text: 3 });
