2b05f15c2b5636002c22b288fbb421e58c7abede
MohammadSeyfiMarandi <mohammadmarandi89@gmail.com>
2018-02-15 14:41:01 +0330

    feat: add style to article header

--- src/public/js/viewpost.js
+++ src/public/js/viewpost.js
@@ -27,5 +27,13 @@ document.addEventListener('scroll', () => {
     normal(header);
   } else {
     minimize(header);
+    bar();
   }
 });
+
+function bar() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
