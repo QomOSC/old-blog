7bd8582612ed693a79fc0e907a8411365f7eb7ab
MohammadSeyfiMarandi <mohammadmarandi89@gmail.com>
2018-02-15 13:07:02 +0330

    feat: add header to article

--- src/public/js/viewpost.js
+++ src/public/js/viewpost.js
@@ -9,3 +9,23 @@ setTimeout(() => {
   md.innerHTML = '';
   md.insertAdjacentHTML('afterend', html);
 }, 100);
+
+const header = document.getElementsByClassName('article-header')[0];
+
+function minimize(arg) {
+  arg.classList.remove('normal');
+  arg.classList.add('minimize');
+}
+
+function normal(arg) {
+  arg.classList.add('normal');
+  arg.classList.remove('minimize');
+}
+
+document.addEventListener('scroll', () => {
+  if (window.scrollY === 0) {
+    normal(header);
+  } else if (window.scrollY !== 0) {
+    minimize(header);
+  }
+});
