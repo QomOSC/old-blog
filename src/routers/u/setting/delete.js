4abf591b6327e74e126e4da33174b3d4c62f2de1
Matin Kaboli <matinkaboli@aol.com>
2018-02-20 13:11:06 +0330

    refactor: use utils/remove functions to remove user data

--- src/routers/u/setting/delete.js
+++ src/routers/u/setting/delete.js
@@ -1,8 +1,8 @@
 import { Router } from 'express';
 
-const { Member, Article, Newsletter } = rootRequire('./models');
+const { Member } = rootRequire('./models');
 const { logged } = rootRequire('./perms');
-const { removeImage } = rootRequire('./utils');
+const { removeImage, remove } = rootRequire('./utils');
 
 const router = new Router();
 
@@ -10,119 +10,24 @@ router.post('/u/setting/delete', logged, async(req, res) => {
 
   const member = await Member.findOne({ _id: req.member.user._id });
 
-  if (member.avatar) {
-    removeImage(member.avatar).then(() => {}).catch(() => {});
-  }
-
-  member.remove().then(async() => {
-
-    const nl = await Newsletter.findOne({ email: member.email });
-
-    if (nl) {
-      nl.remove().then(async() => {
-
-        const articles =
-          await Article.find({ author: req.member.user._id });
-
-        if (articles.length === 0) {
-          req.member.logout();
-          // Done
-          res.json({ type: 0 });
-        } else {
-
-          function* getResponse() {
-            for (const i of articles) {
-              yield new Promise(resolve => {
-
-                if (i.avatar) {
-                  removeImage(i.avatar).then(() => {
-                    i.remove().then(() => {
-                      resolve();
-                    }).catch(() => {});
-                  });
-                } else {
-                  i.remove().then(() => {
-                    resolve();
-                  }).catch(() => {});
-                }
-              });
-            }
-          }
-
-          const iterator = getResponse();
-          (function loop() {
-
-            const next = iterator.next();
-            if (next.done) {
-
-              req.member.logout();
-              // Done
-              res.json({ type: 0 });
-
-              return;
-            }
-
-            next.value.then(loop);
-          })();
-
-        }
-      }).catch(() => {
-        // Error
-        res.json({ type: 2, text: 0 });
-      });
-    } else {
-
-      const articles =
-        await Article.find({ author: req.member.user._id });
-
-      if (articles.length === 0) {
-        req.member.logout();
-        // Done
-        res.json({ type: 0 });
-      } else {
-
-        function* getResponse() {
-          for (const i of articles) {
-            yield new Promise(resolve => {
-
-              if (i.avatar) {
-                removeImage(i.avatar).then(() => {
-                  i.remove().then(() => {
-                    resolve();
-                  }).catch(() => {});
-                });
-              } else {
-                i.remove().then(() => {
-                  resolve();
-                }).catch(() => {});
-              }
-            });
-          }
-        }
-
-        const iterator = getResponse();
-        (function loop() {
-
-          const next = iterator.next();
-          if (next.done) {
-
-            req.member.logout();
-            // Done
-            res.json({ type: 0 });
-
-            return;
-          }
-
-          next.value.then(loop);
-        })();
-
-      }
+  try {
+    if (member.avatar) {
+      await removeImage(member.avatar);
     }
-  }).catch(() => {
-    // Error
-    res.json({ type: 2, text: 0 });
-  });
 
+    await member.remove();
+    await remove.userArticle(member._id);
+    // await remove.userNewsletter(member.email);
+    // await remove.userConference(member._id);
+    // await remove.userGallery(member._id);
+
+    req.member.logout();
+
+    res.json({ type: 0 });
+  } catch (e) {
+    console.log(e);
+    res.json({ type: 2 });
+  }
 });
 
 export default router;
