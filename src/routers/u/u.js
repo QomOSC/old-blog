d1f1b911878d76f7d2b22539561e13b8b0145fbc
Matin Kaboli <matinkaboli@aol.com>
2018-02-23 19:24:27 +0330

    fix: use async/await instead of generators/promises

--- src/routers/u/u.js
+++ src/routers/u/u.js
@@ -12,58 +12,30 @@ router.get('/u', logged, async(req, res) => {
   .limit(9)
   .sort({ createdAt: -1 });
 
-  if (articles.length !== 0) {
+  const allArticles = [];
 
-    const allArts = [];
+  for (const i of articles) {
 
-    function* getResponse() {
+    const oneArticle = {};
 
-      yield new Promise(resolve => {
+    let content = i.content.split('').slice(0, 130);
+    content.push('.', '.', '.');
+    content = content.join('');
 
+    oneArticle.id = i._id;
+    oneArticle.title = i.title;
+    oneArticle.content = content;
+    oneArticle.minutes = i.minutes;
+    oneArticle.avatar = i.avatar;
+    oneArticle.viewers = i.viewers.length;
+    oneArticle.likes = i.likes.length;
 
-        for (const i of articles) {
-
-          const oneArt = {};
-
-          let content = i.content.split('').slice(0, 130);
-          content.push('.', '.', '.');
-          content = content.join('');
-
-          oneArt.id = i._id;
-          oneArt.title = i.title;
-          oneArt.content = content;
-          oneArt.minutes = i.minutes;
-          oneArt.avatar = i.avatar;
-          oneArt.viewers = i.viewers.length;
-          oneArt.likes = i.likes.length;
-
-          allArts.push(oneArt);
-        }
-
-        resolve();
-      });
-    }
-
-    const iterator = getResponse();
-    (function loop() {
-
-      const next = iterator.next();
-      if (next.done) {
-        res.render('u/u.njk', {
-          posts: allArts
-        });
-
-        return;
-      }
-
-      next.value.then(loop);
-    })();
-
-  } else {
-    res.render('u/u.njk', {
-      postEmpty: true
-    });
+    allArticles.push(oneArticle);
   }
+
+  res.render('u/u.njk', {
+    articles: allArticles
+  });
 });
 
 export default router;
--- src/routers/user.js
+++ src/routers/user.js
@@ -14,69 +14,54 @@ router.get('/user/:username', async(req, res) => {
 
     const user = [];
 
-    function* getResponse() {
-      yield new Promise(async resolve => {
-        user.push({
-          _id: member._id,
-          fname: member.fname,
-          lname: member.lname,
-          type: member.type,
-          createdAt: moment(member.createdAt),
-          email: member.email,
-          username: member.username,
-          description: member.description,
-          avatar: member.avatar,
-          postsLength: member.articles.length
-        });
+    user.push({
+      _id: member._id,
+      fname: member.fname,
+      lname: member.lname,
+      type: member.type,
+      createdAt: moment(member.createdAt),
+      email: member.email,
+      username: member.username,
+      description: member.description,
+      avatar: member.avatar,
+      postsLength: member.articles.length
+    });
 
-        const articles = await Article
-          .find({ author: member._id })
-          .limit(12)
-          .sort({ createdAt: -1 });
+    const articles = await Article
+      .find({ author: member._id })
+      .limit(12)
+      .sort({ createdAt: -1 });
 
-        if (articles.length !== 0) {
-          const allArts = [];
+    if (articles.length !== 0) {
+      const allArts = [];
 
-          for (const i of articles) {
-            const oneArt = {};
+      for (const i of articles) {
+        const oneArt = {};
 
-            let content = i.content.split('').slice(0, 110);
-            content.push('.', '.', '.');
-            content = content.join('');
+        let content = i.content.split('').slice(0, 110);
+        content.push('.', '.', '.');
+        content = content.join('');
 
-            oneArt.id = i._id;
-            oneArt.title = i.title;
-            oneArt.content = content;
-            oneArt.minutes = i.minutes;
-            oneArt.avatar = i.avatar;
-            oneArt.viewers = i.viewers.length;
-            oneArt.likes = i.likes.length;
+        oneArt.id = i._id;
+        oneArt.title = i.title;
+        oneArt.content = content;
+        oneArt.minutes = i.minutes;
+        oneArt.avatar = i.avatar;
+        oneArt.viewers = i.viewers.length;
+        oneArt.likes = i.likes.length;
 
-            allArts.push(oneArt);
-          }
-          user.push(allArts);
-        } else {
-          user.push('');
-        }
-        resolve();
-      });
+        allArts.push(oneArt);
+      }
+      user.push(allArts);
+
+    } else {
+      user.push('');
     }
 
-    const iterator = getResponse();
-    (function loop() {
-
-      const next = iterator.next();
-      if (next.done) {
-        res.render('user.njk', {
-          member: user[0],
-          posts: user[1]
-        });
-
-        return;
-      }
-
-      next.value.then(loop);
-    })();
+    res.render('user.njk', {
+      member: user[0],
+      posts: user[1]
+    });
 
   } else {
     res.reply.notFound();
