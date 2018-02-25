6f5eed4cb16de1e282bc78e996ce9ae978845209
Matin Kaboli <matinkaboli@aol.com>
2018-02-26 02:15:56 +0330

    fix: use let over var

--- src/public/js/map.js
+++ src/public/js/map.js
@@ -1,12 +1,12 @@
 function initMap() {
-  var uluru = { lat: 34.6390136, lng: 50.8743435 };
-  var map = new google.maps.Map(document.getElementById('map'), {
+  let uluru = { lat: 34.6390136, lng: 50.8743435 };
+  let map = new google.maps.Map(document.getElementById('map'), {
     zoom: 16,
     center: uluru,
     disableDefaultUI: true
   });
-  var marker = new google.maps.Marker({
+  let marker = new google.maps.Marker({
     position: uluru,
-    map: map
+    map
   });
 }
