16922435cc2444cd9a86517d3b68c6f8645218b6
Matin Kaboli <matinkaboli@aol.com>
2018-02-26 16:19:19 +0330

    refactor: use send() to fetch data

--- src/public/js/add-conference.js
+++ src/public/js/add-conference.js
@@ -1,27 +1,18 @@
-const addConf = document.forms['add-conference'];
-
-addConf.addEventListener('submit', e => {
-  e.preventDefault();
-
-  fetch(addConf.getAttribute('action'), {
-    method: 'POST',
-    credentials: 'include',
-    headers: new Headers({
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-    }),
-    body: JSON.stringify({
-      title: addConf.title.value,
-      description: addConf.description.value
-    })
-  }).then(res => res.json()).then(data => {
-    if (data.type === 2) {
+document.forms['add-conference'].addEventListener('submit', e => {
+  send({
+    url: e.target.getAttribute('action'),
+    method: 'POST'
+  },
+  e,
+  {
+    title: e.target.title.value,
+    description: e.target.description.value
+  }).then(res => {
+    if (res.type === 2) {
       localStorage.setItem('requestforconferencesuccessful', 1);
       window.location.href = '/u';
-    } else if (data.type === 0) {
+    } else if (res.type === 0) {
       iziErr();
     }
-  }).catch(() => {
-    iziErr();
-  });
+  }).catch(() => iziErr());
 });
