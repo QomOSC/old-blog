848fcc34543c3a8a74b41ca71bb07a13d0611d1a
Matin Kaboli <matinkaboli@aol.com>
2018-03-04 01:51:49 +0330

    feat: add a button to attend a conference

--- src/models/Conference.js
+++ src/models/Conference.js
@@ -26,10 +26,10 @@ const schema = new Schema({
     type: Date,
     default: Date.now
   },
-  attender: {
+  attender: [{
     type: String,
     trim: true
-  },
+  }],
   type: { // 1: just requested 2: declined 3: accepted 4: done
     type: Number,
     enum: [1, 2, 3, 4],
