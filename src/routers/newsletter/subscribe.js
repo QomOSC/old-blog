340534f652c5f469965efa0379a936f736055a3e
Matin Kaboli <matinkaboli@aol.com>
2018-03-04 01:55:21 +0330

    style: use return; in newsletters routers

--- src/routers/newsletter/subscribe.js
+++ src/routers/newsletter/subscribe.js
@@ -8,29 +8,30 @@ const router = new Router();
 router.post('/subscribe', async(req, res) => {
   req.body.email = req.body.email.toLowerCase();
 
-  if (validator.e(req.body.email)) {
-
-    const member = await Newsletter.findOne({ email: req.body.email });
-
-    if (member) {
-      // It was in it already
-      res.json({ type: 0, text: 0 });
-    } else {
-      const newMember = new Newsletter({
-        email: req.body.email
-      });
-
-      newMember.save().then(() => {
-        // New email added successfully
-        res.json({ type: 0, text: 1 });
-      }).catch(() => {
-        // Error
-        res.json({ type: 2, text: 0 });
-      });
-    }
-  } else {
+  if (!validator.e(req.body.email)) {
     // Wrong Email
     res.json({ type: 2, text: 1 });
+    return;
+  }
+
+  const member = await Newsletter.findOne({ email: req.body.email });
+
+  if (member) {
+    // It was in it already
+    res.json({ type: 0, text: 0 });
+    return;
+  }
+
+  const newMember = new Newsletter({
+    email: req.body.email
+  });
+
+  try {
+    await newMember.save();
+    // New email added successfully
+    res.json({ type: 0, text: 1 });
+  } catch (e) {
+    res.json({ type: 2, text: 0 });
   }
 });
 
--- src/routers/newsletter/unsubscribe.js
+++ src/routers/newsletter/unsubscribe.js
@@ -13,38 +13,42 @@ router.post('/unsubscribe', async(req, res) => {
   req.body.email = req.body.email.toLowerCase();
   req.body.captcha = req.body.captcha.toLowerCase();
 
-  if (req.body.captcha === req.session.captcha) {
-
-    const member = await Newsletter.findOne({ email: req.body.email });
-
-    if (member) {
-      email.unsubscribe(req.body.email, member._id).then(() => {
-        req.session.captcha = null;
-        res.json({ type: 0 });
-      }).catch(() => {
-        // Error in Sending Email
-        res.json({ type: 2, text: 2 });
-      });
-    } else {
-      // User not found in Newsletter
-      res.json({ type: 2, text: 1 });
-    }
-  } else {
+  if (req.body.captcha !== req.session.captcha) {
     // Wrong Captcha
     res.json({ type: 2, text: 0 });
+    return;
+  }
+
+  const member = await Newsletter.findOne({ email: req.body.email });
+
+  if (!member) {
+    // User not found in Newsletter
+    res.json({ type: 2, text: 1 });
+    return;
+  }
+
+  try {
+    await email.unsubscribe(req.body.email, member._id);
+    req.session.captcha = null;
+    res.json({ type: 0 });
+  } catch (e) {
+    // Error in Sending Email
+    res.json({ type: 2, text: 2 });
   }
 });
 
 router.get('/unsubscribe/:token', async(req, res) => {
   const member = await Newsletter.findOne({ _id: req.params.token });
-  
-  if (member) {
-    member.remove().then(() => {
-      res.render('newsletter/unsubscribe.njk', { done: true });
-    }).catch(() => {
-      res.reply.notFound();
-    });
-  } else {
+
+  if (!member) {
+    res.reply.notFound();
+    return;
+  }
+
+  try {
+    await member.remove();
+    res.render('newsletter/unsubscribe.njk', { done: true });
+  } catch (e) {
     res.reply.notFound();
   }
 });
