2fa77c0b35c04af393216699dbd049f3dfdffaee
Matin Kaboli <matinkaboli@aol.com>
2018-03-05 00:07:57 +0330

    fix: show proper message after adding conference

--- src/public/js/add-conference.js
+++ src/public/js/add-conference.js
@@ -5,12 +5,13 @@ document.forms['add-conference'].addEventListener('submit', e => {
   e,
   {
     title: e.target.title.value,
-    description: e.target.description.value
+    description: e.target.description.value,
+    providers: e.target.providers.value
   }).then(res => {
-    if (res.type === 2) {
+    if (res.type === 0) {
       localStorage.setItem('requestforconferencesuccessful', 1);
       window.location.href = '/u';
-    } else if (res.type === 0) {
+    } else if (res.type === 2) {
       iziErr();
     }
   }).catch(() => iziErr());
