b3d8cf824c2e41eca43e87ad92fdab99f6323533
Matin Kaboli <matinkaboli@aol.com>
2018-03-14 13:33:33 +0330

    fix: show list of not accepted articles properly

--- src/routers/u/article/manage/manage.js
+++ src/routers/u/article/manage/manage.js
@@ -37,4 +37,37 @@ router.get('/u/article/manage', logged, u.admin, async(req, res) => {
   });
 });
 
+router.get('/u/article/manage/:id', logged, u.admin, async(req, res) => {
+  const article = await Article.findOne({ _id: req.params.id, type: 1 }).lean();
+
+  if (!article) {
+    res.reply.notFound();
+    return;
+  }
+
+  delete article.embeds;
+  delete article.type;
+  delete article.viewers;
+  delete article.likes;
+  delete article.minutes;
+  delete article.__v;
+
+  article.createdAt = moment(article.createdAt);
+
+  const author = await Member.findOne({ _id: article.author });
+
+  const authorInfo = {
+    fname: author.fname,
+    lname: author.lname,
+    username: author.username,
+    avatar: author.avatar
+  };
+
+  article.author = authorInfo;
+
+  res.render('u/article/manage/edit.njk', {
+    article
+  });
+});
+
 export default router;
