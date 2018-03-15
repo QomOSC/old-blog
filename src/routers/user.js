e34cc96bf00d5bfd71deca17ae5d0eeb4be8d71b
Matin Kaboli <matinkaboli@aol.com>
2018-03-16 00:02:32 +0330

    fix: not select unnecessary fields

--- src/routers/user.js
+++ src/routers/user.js
@@ -8,22 +8,25 @@ const router = new Router();
 router.get('/user/:username', async(req, res) => {
   req.params.username = req.params.username.toLowerCase();
 
-  const member = await Member.findOne({ username: req.params.username }).lean();
+  const member = await Member
+    .findOne({ username: req.params.username })
+    .select('-__v -submembers -password')
+    .lean();
 
   if (!member) {
     res.reply.notFound();
     return;
   }
-  
+
   member.articles = member.articles.length;
   member.createdAt = moment(member.createdAt);
 
   const articles = await Article
     .find({ author: member._id, type: 2 })
+    .select('-__v -type -author -minutes -embeds')
     .limit(12)
     .sort({ createdAt: -1 })
     .lean();
-
 
   for (const i of articles.keys()) {
     articles[i].content = shorten(articles[i].content);
