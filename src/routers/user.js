6882cfa54a2068542f384ed3006118f70d777792
Matin Kaboli <matinkaboli@aol.com>
2018-03-15 23:49:18 +0330

    refactor: use lean() in user page

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,65 +1,39 @@
 import { Router } from 'express';
 
 const { Member, Article } = rootRequire('./models');
-const { moment } = rootRequire('./utils');
+const { moment, shorten } = rootRequire('./utils');
 
 const router = new Router();
 
 router.get('/user/:username', async(req, res) => {
   req.params.username = req.params.username.toLowerCase();
 
-  const member = await Member.findOne({ username: req.params.username });
+  const member = await Member.findOne({ username: req.params.username }).lean();
 
   if (!member) {
     res.reply.notFound();
     return;
   }
-
-  const user = [];
-
-  user.push({
-    _id: member._id,
-    fname: member.fname,
-    lname: member.lname,
-    type: member.type,
-    createdAt: moment(member.createdAt),
-    email: member.email,
-    username: member.username,
-    description: member.description,
-    avatar: member.avatar,
-    postsLength: member.articles.length
-  });
+  
+  member.articles = member.articles.length;
+  member.createdAt = moment(member.createdAt);
 
   const articles = await Article
     .find({ author: member._id, type: 2 })
     .limit(12)
-    .sort({ createdAt: -1 });
+    .sort({ createdAt: -1 })
+    .lean();
 
-  const allArts = [];
 
-  for (const i of articles) {
-    const oneArt = {};
-
-    let content = i.content.split('').slice(0, 110);
-    content.push('.', '.', '.');
-    content = content.join('');
-
-    oneArt.id = i._id;
-    oneArt.title = i.title;
-    oneArt.content = content;
-    oneArt.minutes = i.minutes;
-    oneArt.avatar = i.avatar;
-    oneArt.viewers = i.viewers.length;
-    oneArt.likes = i.likes.length;
-
-    allArts.push(oneArt);
+  for (const i of articles.keys()) {
+    articles[i].content = shorten(articles[i].content);
+    articles[i].viewers = articles[i].viewers.length;
+    articles[i].likes = articles[i].likes.length;
   }
 
-  user.push(allArts);
-
   res.render('user.njk', {
-    member: user[0],
-    posts: user[1]
+    articles,
+    member
   });
 });
 
