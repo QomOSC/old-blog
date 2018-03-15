ec8ce6983d026b21ab61b19e32cb1b0f8efcb506
Matin Kaboli <matinkaboli@aol.com>
2018-03-16 00:48:24 +0330

    refactor: use lean()

--- src/routers/home.js
+++ src/routers/home.js
@@ -1,73 +1,55 @@
 import { Router } from 'express';
 
 const { Article, Member, Conference } = rootRequire('./models');
-const { moment } = rootRequire('./utils');
+const { moment, shorten } = rootRequire('./utils');
 
 const router = new Router();
 
 router.get('/', async(req, res) => {
 
-  const doc = await Article
+  const articles = await Article
   .find({ type: 2 })
+  .select('-__v -type -minutes -embeds')
   .sort({ createdAt: -1 })
-  .limit(20);
+  .limit(20)
+  .lean();
 
-  const conf = await Conference
+  let lastConference = await Conference
   .find({ type: { $in: [3, 4] } })
+  .select('-attender -createdAt -__v -type')
   .sort({ createdAt: -1 })
-  .limit(1);
+  .limit(1)
+  .lean();
 
-  const articles = [];
+  lastConference = lastConference[0];
 
-  const lastConf = { provider: {} };
+  for (const i of articles.keys()) {
+    articles[i].content = shorten(articles[i].content);
+    articles[i].viewers = articles[i].viewers.length;
+    articles[i].likes = articles[i].likes.length;
+    articles[i].createdAt = moment(articles[i].createdAt);
 
-  for (const i of doc) {
+    const member = await Member
+      .findOne({ _id: articles[i].author })
+      .select('-__v -_id -password -submembers -articles -createdAt -type');
 
-    let content = i.content.split('').slice(0, 130);
-    content.push('.', '.', '.');
-    content = content.join('');
-
-    const oneArticle = {
-      _id: i._id,
-      title: i.title,
-      createdAt: moment(i.createdAt),
-      likes: i.likes.length,
-      viewers: i.viewers.length,
-      avatar: i.avatar,
-      author: {},
-      content
-    };
-
-    const member = await Member.findOne({ _id: i.author });
-
-    if (member) {
-      oneArticle.author.fname = member.fname;
-      oneArticle.author.lname = member.lname;
-      oneArticle.author.username = member.username;
-      oneArticle.author.avatar = member.avatar;
-
-      articles.push(oneArticle);
-    }
+    articles[i].author = member;
   }
 
-  if (conf.length) {
-    lastConf._id = conf[0]._id;
-    lastConf.title = conf[0].title;
-    lastConf.description = conf[0].description;
-    lastConf.avatar = conf[0].avatar;
-    lastConf.createdAt = moment(conf[0].createdAt);
+  if (lastConference) {
+    lastConference.createdAt = moment(lastConference.createdAt);
 
-    const provider = await Member.findOne({ _id: conf[0].author });
+    const provider = await Member.findOne({ _id: lastConference.author });
 
-    if (provider) {
-      lastConf.provider.fname = provider.fname;
-      lastConf.provider.lname = provider.lname;
-      lastConf.provider.username = provider.username;
-      lastConf.provider.avatar = provider.avatar;
-    }
-  } else { lastConf.empty = true; }
+    lastConference.provider = provider;
+  } else {
+    lastConference.empty = true;
+  }
 
-  res.render('home.njk', { posts: articles, lastConf });
+  res.render('home.njk', {
+    lastConf: lastConference,
+    articles
+  });
 });
 
 export default router;
