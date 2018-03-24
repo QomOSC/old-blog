7e2d03675cdd54bd7eea889416be90d551a70c07
Matin Kaboli <matinkaboli@aol.com>
2018-03-25 00:10:58 +0430

    feat: store.js

---
+++ src/client/store.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
+
+const store = createStore(reducers, applyMiddleware(thunk));
+
+if (process.env.NODE_ENV === 'development') {
+  window.store = store;
+}
+
+export default store;
