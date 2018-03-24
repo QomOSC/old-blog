c8e2b03f7dd6f51490109471dc05dd329065c184
Matin Kaboli <matinkaboli@aol.com>
2018-03-25 00:19:35 +0430

    feat: add styles

--- src/client/client.js
+++ src/client/client.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
+import App from './components/App';
+
 import store from './store';
 
 import 'izitoast/dist/css/iziToast.min.css';
+import './css/font-fa.less';
+import './css/index.less';
 
 window.onload = () => {
   render(
     <Provider store={store}>
-      <h1>Hello World</h1>
+      <App />
     </Provider>,
     document.getElementById('root')
   );
