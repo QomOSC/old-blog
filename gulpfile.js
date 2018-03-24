f3a286dc503b5bdbf69fb51ebed34e4a97eb5e52
Matin Kaboli <matinkaboli@aol.com>
2018-03-24 22:43:45 +0430

    feat: gulpfile configs

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+const gulp = require('gulp');
+const webpack = require('webpack-stream');
+const del = require('del');
+const lint = require('gulp-eslint');
+
+gulp.task('clean', () =>
+  del([
+    'build/**',
+    '!build',
+    '!build/static',
+    '!build/static/uploads',
+    '!build/static/uploads/**'
+  ])
+);
+
+gulp.task('copy', () =>
+  gulp.src([
+    'src/index.html'
+  ])
+  .pipe(gulp.dest('build/'))
+);
+
+gulp.task('dev:client', () =>
+  gulp.src('src/client/client.js')
+  .pipe(webpack(require('./webpack/client/dev.js')))
+  .pipe(gulp.dest('build/static/js'))
+);
+
+gulp.task('prod:client', () =>
+  gulp.src('src/client/client.js')
+  .pipe(webpack(require('./webpack/client/prod.js')))
+  .pipe(gulp.dest('build/static/js'))
+);
+
+gulp.task('dev:server', () =>
+  gulp.src('src/app.js')
+  .pipe(webpack(require('./webpack/server/dev.js')))
+  .pipe(gulp.dest('build/'))
+);
+
+gulp.task('prod:server', () =>
+  gulp.src('src/app.js')
+  .pipe(webpack(require('./webpack/server/prod.js')))
+  .pipe(gulp.dest('build/'))
+);
+
+gulp.task('lint', () =>
+  gulp.src('src/**/*.js')
+  .pipe(lint())
+  .pipe(lint.format())
+  .pipe(lint.failAfterError())
+);
+
+gulp.task('dev', ['clean', 'copy', 'dev:client', 'dev:server']);
+gulp.task('prod', ['clean', 'copy', 'prod:client', 'prod:server']);
