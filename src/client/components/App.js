9793676e86d714891d6714389b2f10178f4912d5
Matin Kaboli <matinkaboli@aol.com>
2018-03-25 00:19:57 +0430

    feat: add components

---
+++ src/client/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+
+import Home from './Home';
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default App;
