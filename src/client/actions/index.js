7dbdf0a2b6dda3931e3480e6ac8a6a1c8524fafc
Matin Kaboli <matinkaboli@aol.com>
2018-03-25 16:16:13 +0430

    fix: add CAPTCHA to reducer

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -1,4 +1,5 @@
 export default {
+  CAPTCHA: 'CAPTCHA',
   user: {
     ADD: 'user/ADD'
   }
--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
+import captcha from './captcha';
 import user from './user';
 
 export default combineReducers({
+  captcha,
   user
 });
