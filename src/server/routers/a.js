6e138afa5bcbdc48037d8fefc940c0dbab571de1
Matin Kaboli <matinkaboli@aol.com>
2018-03-25 16:17:34 +0430

    del: unnecessary /a router

--- src/server/routers/a.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-const router = new Router();
-
-router.get('/a', (req, res) => {
-  res.send('Hello');
-});
-
-export default router;
--- src/server/routers/index.js
+++ src/server/routers/index.js
@@ -1,4 +1,3 @@
 export default [
-  require('./a'),
   require('./captcha')
 ];
