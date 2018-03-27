10851f7032eb85bd0145c267481031a6b11ea98d
Matin Kaboli <matinkaboli@aol.com>
2018-03-28 01:25:18 +0430

    feat: change links in header when user logs in

--- src/client/components/Public/Includes/Header/index.js
+++ src/client/components/Public/Includes/Header/index.js
@@ -1,17 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import styles from './index.less';
 
 
 class Header extends Component {
   render() {
+    console.log(this.props.logged);
     return (
       <header className={styles.header}>
         <Link to='/'>جامعه متن باز قم</Link>
         <ul>
-          <li><Link to='/login'>ورود</Link></li>
-          <li><Link to='/signup'>ثبت نام</Link></li>
+          {this.props.logged ? <Fragment>
+            <li><Link to='/signup'>داشبورد</Link></li>
+          </Fragment> :
+          <Fragment>
+            <li><Link to='/login'>ورود</Link></li>
+            <li><Link to='/signup'>ثبت نام</Link></li>
+          </Fragment>}
+
           <li><Link to='/contact'>تماس با ما</Link></li>
         </ul>
       </header>
@@ -19,4 +27,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+export default connect(
+  state => ({
+    logged: state.user.logged
+  })
+)(Header);
