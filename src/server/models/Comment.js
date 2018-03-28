1a366c996d4132361ebaa42292dd374429a1ad5b
Matin Kaboli <matinkaboli@aol.com>
2018-03-28 23:47:52 +0430

    feat: add Models of DB

---
+++ src/server/models/Comment.js
@@ -0,0 +1,70 @@
+import mongoose, { Schema } from 'mongoose';
+
+import { validator } from '../utils';
+
+const schema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    maxlength: 80,
+    required: [true, 'name']
+  },
+  email: {
+    type: String,
+    lowercase: true,
+    trim: true,
+    validate: {
+      validator(v) {
+        return validator.e(v);
+      },
+      message: 'It is not a valid email'
+    },
+    required: [true, 'email'],
+    maxlength: 200
+  },
+  title: {
+    type: String,
+    maxlength: 100,
+    trim: true
+  },
+  description: {
+    type: String,
+    required: [true, 'content'],
+    trim: true
+  },
+  admin: {
+    type: String,
+    trim: true
+  },
+  answer: {
+    type: String,
+    trim: true
+  },
+  type: {
+    type: Number,
+    enum: [1, 2], // 1: requested, 2: accepted
+    required: [true, 'type'],
+    trim: true,
+    default: 1
+  },
+  contact: {
+    type: Boolean, // true = it's from /contact,   false = it's from an article
+    required: [true, 'contact'],
+    trim: true,
+    default: true
+  },
+  article: {
+    type: Number,
+    trim: true
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'Member'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+export default mongoose.model('Comment', schema);
