a2d7189f01120e015445a34775838917493371b0
Matin Kaboli <matinkaboli@aol.com>
2018-03-29 23:24:48 +0430

    feat: Home page

--- src/client/components/Public/Home/index.js
+++ src/client/components/Public/Home/index.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
 
+import Box from 'Root/components/Utils/Box';
+
+import styles from './index.less';
+
 
 class Home extends Component {
   render() {
-    return <h1>Hello World!</h1>;
+    return (
+      <Box>
+        <div className={styles.contact}>
+          <div>
+            <h1>اگر سوالی دارید بپرسید</h1>
+            <p>شما میتوانید تمامی انتقادات و پیشنهادات خود را ارسال کنید</p>
+          </div>
+
+          <div>
+            <input
+              type='text'
+              refs='name'
+              placeholder='نام'
+            />
+          </div>
+        </div>
+      </Box>
+    );
   }
 }
 
