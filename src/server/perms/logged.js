73d984d4a97e832a9d595fbff57085f832c2c489
Matin Kaboli <matinkaboli@aol.com>
2018-03-30 01:21:07 +0430

    del: permission files

--- src/server/perms/logged.js
+++
@@ -1,3 +0,0 @@
-export default (req, res, next) => {
-  req.session.user ? next() : res.json({ type: 3 });
-};
--- src/server/perms/login.js
+++
@@ -1,3 +0,0 @@
-export default (req, res, next) => {
-  req.session.user ? res.json({ type: 3 }) : next();
-};
