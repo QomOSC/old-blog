89749f0faa78100fb7e3e786146858c3d0a8708e
Matin Kaboli <matinkaboli@aol.com>
2018-04-01 19:12:10 +0430

    feat: add some styles to /user/:id

--- src/client/components/Public/User/index.js
+++ src/client/components/Public/User/index.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 import { withRouter, Redirect } from 'react-router-dom';
 import nprogress from 'nprogress';
 
+import moment from 'Root/js/moment';
 import send from 'Root/js/send';
+import bind from 'Root/js/bind';
+
+import userDefault from 'Root/images/u.png';
+import styles from './index.less';
+
 
 class User extends Component {
   state = {
@@ -20,6 +26,33 @@ class User extends Component {
     });
   }
 
+  @bind
+  role() {
+    if (this.state.data.user.type === 1) {
+      return 'در انتظار پذیرش';
+    } else if (this.state.data.user.type === 2) {
+      return 'کاربر معمولی';
+    } else if (this.state.data.user.type === 3) {
+      return 'مدیر';
+    }
+    return 'مدیر کل';
+  }
+
+  @bind
+  renderImage() {
+    if (this.state.data.user.avatar) {
+      return <img
+        src={`/static/image/${this.state.data.user.avatar}`}
+        className={styles.userImage}
+      />;
+    }
+
+    return <img
+      src={userDefault}
+      className={styles.userImage}
+    />;
+  }
+
   render() {
     if (!this.state.data) {
       return <h1>در حال گرفتن اطلاعات</h1>;
@@ -30,15 +63,22 @@ class User extends Component {
     }
 
     return (
-      <div>
-        <p>name: {this.state.data.user.name}</p>
-        <p>username: {this.state.data.user.username}</p>
-        <p>email: {this.state.data.user.email}</p>
-        <p>type: {this.state.data.user.type}</p>
-        <p>avatar: {this.state.data.user.avatar}</p>
-        <p>description: {this.state.data.user.description}</p>
-        <p>created At: {this.state.data.user.createdAt}</p>
-        <p>articles: {this.state.data.user.articles}</p>
+      <div className={styles.container}>
+        <div className={styles.user}>
+          <p>{this.state.data.user.name}</p>
+          <p>{this.state.data.user.email}</p>
+          <p>{this.role()}</p>
+          {this.renderImage()}
+          {this.state.data.user.description && <p>
+            درباره: {this.state.data.user.description}
+          </p>}
+          <p>عضو شده در:‌{moment(this.state.data.user.createdAt)}</p>
+          <p>تعداد مقالات: {this.state.data.user.articles}</p>
+        </div>
+
+        <div className={styles.articles}>
+          <h1>Articles</h1>
+        </div>
       </div>
     );
   }
