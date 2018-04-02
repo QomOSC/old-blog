c1f32217637fb67eeeb8449ebde200e8b78d7ad5
Matin Kaboli <matinkaboli@aol.com>
2018-04-02 22:28:58 +0430

    feat: change the style of article template

--- src/client/components/Panel/Articles/Articles/index.js
+++ src/client/components/Panel/Articles/Articles/index.js
@@ -5,7 +5,15 @@ import Article from 'Root/components/Utils/Article';
 class Articles extends Component {
   render() {
     return (
-      <Article />
+      <Article
+        user={{ avatar: 'aaa', username: 'matin', name: 'متین' }}
+        art={{
+          createdAt: 'سه روز پیش',
+          minutes: 'سه دقیقه',
+          title: 'چرا باید نحوه زندگی کردنمان را تغییر دهیم',
+          avatar: 'aaa'
+        }}
+      />
     );
   }
 }
--- src/client/components/Utils/Article/index.js
+++ src/client/components/Utils/Article/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import userDefault from 'Root/images/u.png';
 import styles from './index.less';
@@ -8,7 +9,20 @@ class Article extends Component {
   render() {
     return (
       <div className={styles.article}>
-        <img src={userDefault} />
+        <div className={styles.user}>
+          {this.props.user.avatar ?
+            <img src={this.props.user.avatar} alt='عکس کاربر'/> :
+            <img src={userDefault} alt='عکس کاربر' />
+          }
+          <div>
+            <Link to={`/user/${this.props.user.username}`}>
+              {this.props.user.name || 'سلام به روی ماهت'}
+            </Link>
+            <p>{this.props.art.createdAt} - {this.props.art.minutes} خواندن</p>
+          </div>
+        </div>
+        {this.props.art.title && <h1>{this.props.art.title}</h1>}
+        {this.props.art.avatar && <img src={this.props.art.avatar} />}
       </div>
     );
   }
