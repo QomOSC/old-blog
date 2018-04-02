9aacdeb2c76af9155a881863e261b63efb7077eb
Matin Kaboli <matinkaboli@aol.com>
2018-04-03 02:46:32 +0430

    feat: add Article

---
+++ src/client/components/Panel/Articles/Add/index.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import SimpleMDE from 'react-simplemde-editor';
+import izitoast from 'izitoast';
+
+import bind from 'Root/js/bind';
+
+import 'Root/css/simplemde.less';
+import styles from './index.less';
+
+
+class AddArticle extends Component {
+  state = {
+    content: '',
+    avatar: ''
+  };
+
+  @bind
+  post() {
+    if (!this.refs.title.value || !this.state.content) {
+      izitoast.warning({
+        rtl: true,
+        title: 'مقادیر کافی نیستند'
+      });
+      return;
+    }
+
+    console.log(this.state);
+  }
+
+  @bind
+  updateAvatar() {
+    let reader = new FileReader();
+    let file = this.refs.file.files[0];
+    let type = file.type.split('/')[1];
+
+    reader.addEventListener('loadend', () => {
+      if (file.size > 1048576) {
+        izitoast.warning({
+          rtl: true,
+          title: 'حجم فایل حداکثر می تواند ۱ مگابایت باشد.'
+        });
+
+        return;
+      }
+
+      if (!['jpg', 'jpeg', 'png'].includes(type)) {
+        izitoast.warning({
+          rtl: true,
+          title: 'فرمت فایل باید jpg یا png باشد'
+        });
+
+        return;
+      }
+
+    });
+
+    reader.readAsBinaryString(this.refs.file.files[0]);
+
+    this.setState({ avatar: this.refs.file.files[0] });
+    console.log('Changed');
+  }
+
+  @bind
+  openInput() {
+    this.refs.file.click();
+  }
+
+  @bind
+  handle(content) {
+    this.setState({ content });
+  }
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <input
+          type='text'
+          placeholder='عنوان'
+          ref='title'
+        />
+        <SimpleMDE
+          ref='content'
+          onChange={this.handle}
+          options={{
+            spellChecker: false
+          }}
+        />
+
+        <input
+          type='file'
+          ref='file'
+          className={styles.avatarInput}
+          onChange={this.updateAvatar} />
+
+        <button
+          onClick={this.openInput}
+          color='blue'>
+          اضافه کردن عکس
+        </button>
+
+        <button onClick={this.post}>ثبت</button>
+      </div>
+    );
+  }
+}
+
+export default AddArticle;
--- src/client/components/Panel/Articles/index.js
+++ src/client/components/Panel/Articles/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
+import AddArticle from './Add';
 import Articles from './Articles';
 
 
@@ -8,6 +9,7 @@ class Article extends Component {
   render() {
     return (
       <Switch>
+        <Route exact path='/panel/articles/add' component={AddArticle} />
         <Route exact path='/panel/articles' component={Articles} />
         <Route>
           <Redirect to='/notfound' />
