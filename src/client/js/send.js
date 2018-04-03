6d6844124be0b73fdc2d477a9edc3e275e66dc66
Matin Kaboli <matinkaboli@aol.com>
2018-04-03 21:16:10 +0430

    feat: add FormData send

--- src/client/js/send.js
+++ src/client/js/send.js
@@ -1,5 +1,5 @@
-const send = (url, data) => new Promise((resolve, reject) => {
-  fetch(url, {
+const send = (url, data, formData) => new Promise((resolve, reject) => {
+  const config = {
     method: 'POST',
     credentials: 'include',
     headers: new Headers({
@@ -9,7 +9,20 @@ const send = (url, data) => new Promise((resolve, reject) => {
     body: JSON.stringify({
       ...data
     })
-  }).then(res => res.json()).then(res => {
+  };
+
+  if (formData === 'formData') {
+    const fd = new FormData();
+
+    for (const v of Object.entries(data)) {
+      fd.append(v[0], v[1]);
+    }
+
+    config.body = fd;
+    config.headers = {};
+  }
+
+  fetch(url, config).then(res => res.json()).then(res => {
     resolve(res);
   }).catch(e => {
     reject(e);
