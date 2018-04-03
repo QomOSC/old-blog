0c19ed7d9fe5a8e5cfe7f33c0979637342383899
Matin Kaboli <matinkaboli@aol.com>
2018-04-03 21:54:52 +0430

    feat: use storage for saving images

---
+++ src/server/utils/storage.js
@@ -0,0 +1,24 @@
+import multer from 'multer';
+
+import random from './random';
+
+import config from 'Root/config';
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, config.uploadDir);
+  },
+  async filename(req, file, cb) {
+    const r = await random(16);
+
+    const extension = file.mimetype.split('/')[1];
+
+    if (extension === 'jpeg' || extension === 'png') {
+      cb(null, r + Date.now() + '.' + extension);
+    } else {
+      cb(new Error('not an image'));
+    }
+  }
+});
+
+export default storage;
