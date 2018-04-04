119f9886c7e3cdbbab5e470e2bfd6823060656df
Matin Kaboli <matinkaboli@aol.com>
2018-04-04 16:13:01 +0430

    feat: use loading progress in user/id

--- src/client/components/Public/User/index.js
+++ src/client/components/Public/User/index.js
@@ -84,7 +84,22 @@ class User extends Component {
 
   render() {
     if (!this.state.data) {
-      return <h1>در حال گرفتن اطلاعات</h1>;
+      return <div className='loadingContainer'>
+        <div className="sk-fading-circle">
+          <div className="sk-circle1 sk-circle" />
+          <div className="sk-circle2 sk-circle" />
+          <div className="sk-circle3 sk-circle" />
+          <div className="sk-circle4 sk-circle" />
+          <div className="sk-circle5 sk-circle" />
+          <div className="sk-circle6 sk-circle" />
+          <div className="sk-circle7 sk-circle" />
+          <div className="sk-circle8 sk-circle" />
+          <div className="sk-circle9 sk-circle" />
+          <div className="sk-circle10 sk-circle" />
+          <div className="sk-circle11 sk-circle" />
+          <div className="sk-circle12 sk-circle" />
+        </div>
+      </div>;
     }
 
     if (this.state.data.user === null) {
