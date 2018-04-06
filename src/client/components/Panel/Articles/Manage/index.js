c71e5eac62c19c5010d80c05ce449c351fd499b4
Matin Kaboli <matinkaboli@aol.com>
2018-04-06 17:47:51 +0430

    feat: Manage Articles

--- src/client/components/Panel/Articles/Manage/index.js
+++ src/client/components/Panel/Articles/Manage/index.js
@@ -2,25 +2,66 @@ import React, { Component } from 'react';
 
 import gql from 'Root/js/gql';
 
+import LoadingProgress from 'Root/components/Utils/LoadingProgress';
+import Article from 'Root/components/Utils/Article';
+
+import styles from './index.less';
+
 
 class Manage extends Component {
+  state = {
+    data: undefined
+  };
+
   componentDidMount() {
     const query = `
       query {
         articles(type: 1) {
+          createdAt
+          minutes
+          avatar
           title
+
+          user {
+            description
+            createdAt
+            articles
+            username
+            avatar
+            email
+            type
+            name
+          }
         }
       }
     `;
 
     gql(query).then(data => {
-      console.log(data);
+      this.setState({ data: data.data });
     });
   }
 
   render() {
+    if (!this.state.data) {
+      return <LoadingProgress />;
+    }
+
     return (
-      <h1>Manage</h1>
+      <div className={styles.container}>
+        {this.state.data.articles.length && <h1>مقالات جدید</h1>}
+
+        <div className={styles.articles}>
+          {this.state.data.articles.map((v, i) =>
+            <Article
+              key={i}
+              user={{ ...v.user }}
+              art={{ ...v }}
+            >
+              <h1>Hello</h1>
+            </Article>
+          )}
+        </div>
+      </div>
     );
   }
 }
