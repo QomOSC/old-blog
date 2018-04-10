50501277f43265511675b3fc063ba5d14a7872a8
Matin Kaboli <matinkaboli@aol.com>
2018-04-10 23:49:52 +0430

    feat: remove everything related to article when rejecting it

--- src/server/routers/user/articles/reject.js
+++ src/server/routers/user/articles/reject.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 
 import Article from 'Root/models/Article';
+import User from 'Root/models/User';
+import Tag from 'Root/models/Tag';
 
 import { admin } from 'Root/perms';
+
+import removeImage from 'Root/utils/removeImage';
 
 const router = new Router();
 
@@ -14,6 +18,22 @@ router.post('/panel/articles/reject', admin, async (req, res) => {
       res.json({ type: 2, text: 0 });
       return;
     }
+
+    const author = await User.findById(article.author);
+
+    author.articles.splice(article._id);
+
+    await author.save();
+
+    const tags = await Tag.find({ article: article._id });
+
+    if (tags.length) {
+      for (const i of tags) {
+        await i.remove();
+      }
+    }
+
+    await removeImage(article.avatar);
 
     await article.remove();
 
