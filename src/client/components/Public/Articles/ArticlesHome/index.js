cb9406a2f941d1ad03f4fd6cdabd051ce94460ab
Matin Kaboli <matinkaboli@aol.com>
2018-04-10 23:24:21 +0430

    feat: show articles in /articles

--- src/client/components/Public/Articles/ArticlesHome/index.js
+++ src/client/components/Public/Articles/ArticlesHome/index.js
@@ -1,12 +1,71 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+
+import gql from 'Root/js/gql';
+
+import LoadingProgress from 'Root/components/Utils/LoadingProgress';
+import Article from 'Root/components/Utils/Article';
+
+import styles from './index.less';
 
 
 class ArticlesHome extends Component {
+  state = {
+    articles: undefined
+  };
+
+  componentDidMount() {
+    const query = `
+      query {
+        articles(type: 2) {
+          createdAt
+          minutes
+          avatar
+          title
+          _id
+
+          user {
+            description
+            createdAt
+            articles
+            username
+            avatar
+            email
+            type
+            name
+          }
+        }
+      }
+    `;
+
+    gql(query).then(data => {
+      this.setState({ articles: data.data.articles });
+    });
+  }
+
   render() {
+    if (!this.state.articles) {
+      return <LoadingProgress />;
+    }
+
     return (
-      <h1>Home</h1>
+      <div className={styles.container}>
+        {this.state.articles.length ?
+          <h1>مقالات اخیر</h1> :
+          <h1>مقاله ای وجود ندارد</h1>
+        }
+        
+        {this.state.articles.map((v, i) =>
+          <Article
+            key={i}
+            user={{ ...v.user }}
+            art={{ ...v }}
+            id={v._id}
+          />
+        )}
+      </div>
     );
   }
 }
 
-export default ArticlesHome;
+export default withRouter(ArticlesHome);
