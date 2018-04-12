0c16b6f3e6f8270359185e2c65903d94906b93a7
Matin Kaboli <matinkaboli@aol.com>
2018-04-12 12:38:38 +0430

    feat: show articles with same tag in /tag/tagname

--- src/client/components/Public/Tag/index.js
+++ src/client/components/Public/Tag/index.js
@@ -1,11 +1,77 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+
+import gql from 'Root/js/gql';
+
+import LoadingProgress from 'Root/components/Utils/LoadingProgress';
+import Article from 'Root/components/Utils/Article';
+
+import styles from './index.less';
 
 
 class Tag extends Component {
+  state = {
+    articles: undefined
+  }
+
+  componentDidMount() {
+    const query = `
+      query {
+        tag(tagname: "${this.props.match.params.tagname}") {
+          articles {
+            createdAt
+            minutes
+            avatar
+            title
+            _id
+
+            user {
+              description
+              createdAt
+              articles
+              username
+              avatar
+              email
+              type
+              name
+            }
+          }
+        }
+      }
+    `;
+
+    gql(query).then(data => {
+      if (data.data.tag) {
+        this.setState({ articles: data.data.tag.articles });
+        return;
+      }
+
+      this.props.history.push('/notfound');
+    });
+  }
+
   render() {
-    console.log(this.props.match.params.tagname);
-    return <p>Hi</p>;
+    if (!this.state.articles) {
+      return <LoadingProgress />;
+    }
+
+    return (
+      <div className={styles.container}>
+        <h1>برچسب: {this.props.match.params.tagname}</h1>
+
+        <div className={styles.articles}>
+          {this.state.articles.map((v, i) =>
+            <Article
+              user={{ ...v.user }}
+              art={{ ...v }}
+              _id={v._id}
+              key={i}
+            />
+          )}
+        </div>
+      </div>
+    );
   }
 }
 
-export default Tag;
+export default withRouter(Tag);
