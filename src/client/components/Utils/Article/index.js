a7dbfbf43a922bc258a6f6f5bbd98d858837599c
MohammadSeyfiMarandi <mohammadmarandi89@gmail.com>
2018-06-12 03:17:15 +0430

    fix: style :)

--- src/client/components/Utils/Article/index.js
+++ src/client/components/Utils/Article/index.js
@@ -11,6 +11,13 @@ class Article extends Component {
   render() {
     return (
       <div className={styles.article}>
+
+        <Link to={`/articles/${this.props.id}`}>
+          {this.props.art.avatar ? <img src=
+            {`/static/uploads/${this.props.art.avatar}`} /> : ''}
+          {this.props.art.title ? <h1>{this.props.art.title}</h1> : ''}
+          <p>عنوان Skull & Bones یکی دیگر از بازی های مهمی بود که طی کنفرانس E3 2018 یوبی سافت شاهد انتشار تریلری تازه ای از آن بودیم. تریلر جدید این بازی نشان از نبردهای دریایی عظیمی هستند که برای پیروزی در آن ها نیاز به همکاری با دیگران خواهید داشت.</p>
+        </Link>
         <div className={styles.user}>
           {this.props.user.avatar ?
             <img src=
@@ -20,19 +27,12 @@ class Article extends Component {
             <img src={userDefault} alt='عکس کاربر' />
           }
           <div>
-            <Link to={`/user/${this.props.user.username}`}>
-              {this.props.user.name || 'سلام به روی ماهت'}
+          <Link to={`/user/${this.props.user.username}`}>
+              {this.props.user.name}
             </Link>
-            <p>{moment(new Date(this.props.art.createdAt))} -
-              &nbsp;{this.props.art.minutes} دقیقه خواندن</p>
+            <a>ادامه مطلب  . . .</a>
           </div>
         </div>
-        <Link to={`/articles/${this.props.id}`}>
-          {this.props.art.avatar ? <img src=
-            {`/static/uploads/${this.props.art.avatar}`} /> : ''}
-          {this.props.art.title ? <h1>{this.props.art.title}</h1> : ''}
-
-        </Link>
         <div className={styles.others}>
           {this.props.children}
         </div>
