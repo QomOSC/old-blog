81c56e430dbe59daa916447e48d7aced398d3891
Matin Kaboli <matinkaboli@aol.com>
2018-06-12 03:43:42 +0430

    fix: delete some shitty parts

--- src/client/components/Utils/Article/index.js
+++ src/client/components/Utils/Article/index.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-import moment from 'Root/js/moment';
-
 import userDefault from 'Root/images/u.png';
 import styles from './index.less';
 
@@ -13,11 +11,20 @@ class Article extends Component {
       <div className={styles.article}>
 
         <Link to={`/articles/${this.props.id}`}>
-          {this.props.art.avatar ? <img src=
-            {`/static/uploads/${this.props.art.avatar}`} /> : ''}
-          {this.props.art.title ? <h1>{this.props.art.title}</h1> : ''}
-          <p>عنوان Skull & Bones یکی دیگر از بازی های مهمی بود که طی کنفرانس E3 2018 یوبی سافت شاهد انتشار تریلری تازه ای از آن بودیم. تریلر جدید این بازی نشان از نبردهای دریایی عظیمی هستند که برای پیروزی در آن ها نیاز به همکاری با دیگران خواهید داشت.</p>
+          {this.props.art.avatar ?
+            <img src={`/static/uploads/${this.props.art.avatar}`} /> :
+            ''
+          }
+
+          {this.props.art.title ?
+            <h1>{this.props.art.title}</h1> :
+            ''
+          }
+
+          <p>متن</p>
+
         </Link>
+
         <div className={styles.user}>
           {this.props.user.avatar ?
             <img src=
@@ -26,13 +33,15 @@ class Article extends Component {
 
             <img src={userDefault} alt='عکس کاربر' />
           }
+
           <div>
-          <Link to={`/user/${this.props.user.username}`}>
-              {this.props.user.name}
+            <Link to={`/user/${this.props.user.username}`}>
+                {this.props.user.name}
             </Link>
             <a>ادامه مطلب  . . .</a>
           </div>
         </div>
+
         <div className={styles.others}>
           {this.props.children}
         </div>
